cdd9a1729babdd8ebe349a4a872a3bedb0f3c899
Nikolai Ivanenko <nivanenko@outlook.com>
2019-03-08 22:45:37 +0200

    license

--- test/strutl.hxx
+++ test/strutl.hxx
@@ -1,3 +1,8 @@
+// Copyright (c) 2017-2019 oknenavin.
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
 #ifndef STRUTL_HXX_
 #define STRUTL_HXX_
 
