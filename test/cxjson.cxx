306cca2ad8253c572ac198f93fa65b59850cbc3b
oknenavin <oknenavin@outlook.com>
2019-04-07 00:47:07 +0300

    cxjson: error::recursion_depth_exceeded masked test/cxjson: error::invalid test test/cxjson: error::recursion_depth_exceeded test + error handling

--- test/cxjson.cxx
+++ test/cxjson.cxx
@@ -151,8 +151,7 @@ static unsigned self() {
 #   define CHECK(c) ++a_; if (!(c))\
         fprintf(stderr, "must pass, but failed: at %s:%li\n", __FILE__, (long)__LINE__), fflush(stderr), ++f_;\
         CXON_ASSERT((c), "check failed");
-    {
-        //using node = cxjson::node;
+    {   //using node = cxjson::node;
 
         char const s[] = "{\"array0\":[{\"1\":1},{\"2\":2}],\"array1\":[\"string\",3,{\"bool\":true,\"null\":null},null],\"number\":4}";
         node jns;
@@ -188,6 +187,14 @@ static unsigned self() {
             std::string const s1 = cxjson::pretty(jno);
             CHECK(s0 == s1);
         }
+        {   node jn;
+            auto const r = cxon::from_chars(jn, "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[");
+            CHECK(!r && r.ec == cxjson::error::recursion_depth_exceeded);
+        }
+        {   node jn;
+            auto const r = cxon::from_chars(jn, "~");
+            CHECK(!r && r.ec == cxjson::error::invalid);
+        }
         {   using namespace cxjson;
             std::error_condition ec;
             ec = error::ok;
@@ -203,186 +210,186 @@ static unsigned self() {
                     CXON_ASSERT(ec.message() == "unknown error", "check failed");
 #           endif
         }
-        {   // ex1
-            using my_type = std::map<std::string, std::vector<int>>;
+    }
+    {   // ex1
+        using my_type = std::map<std::string, std::vector<int>>;
 
-            my_type mv1 = { {"even", {2, 4, 6}}, {"odd", {1, 3, 5}} },
-                    mv2;
-            std::string json;
-                cxon::to_chars(json, mv1);
-                cxon::from_chars(mv2, json);
-            CHECK(mv1 == mv2);
+        my_type mv1 = { {"even", {2, 4, 6}}, {"odd", {1, 3, 5}} },
+                mv2;
+        std::string json;
+            cxon::to_chars(json, mv1);
+            cxon::from_chars(mv2, json);
+        CHECK(mv1 == mv2);
 
-            std::string const pretty_json = cxon::pretty(json);
-        }
-        {   // ex2
-            my_type mv1 = { {2, 4, 6}, {1, 3, 5} },
-                    mv2;
-            std::string json;
-                cxon::to_chars(json, mv1);
-                cxon::from_chars(mv2, json);
-            CHECK(mv1 == mv2);
-        }
-        {   // ex3
-            using node = cxjson::node;
+        std::string const pretty_json = cxon::pretty(json);
+    }
+    {   // ex2
+        my_type mv1 = { {2, 4, 6}, {1, 3, 5} },
+                mv2;
+        std::string json;
+            cxon::to_chars(json, mv1);
+            cxon::from_chars(mv2, json);
+        CHECK(mv1 == mv2);
+    }
+    {   // ex3
+        using node = cxjson::node;
         
-            char const s0[] = "{\"even\":[2,4,6],\"odd\":[1,3,5]}";
-            node const n0 = node::object {
-                { "even", node::array { 2, 4, 6 } },
-                { "odd", node::array { 1, 3, 5 } }
-            };
+        char const s0[] = "{\"even\":[2,4,6],\"odd\":[1,3,5]}";
+        node const n0 = node::object {
+            { "even", node::array { 2, 4, 6 } },
+            { "odd", node::array { 1, 3, 5 } }
+        };
 
-            node n1; // read
-                cxon::from_chars(n1, s0);
-            CHECK(n1 == n0);
+        node n1; // read
+            cxon::from_chars(n1, s0);
+        CHECK(n1 == n0);
 
-            std::string s1; // write
-                cxon::to_chars(s1, n0);
-            CHECK(s1 == s0);
-        }
-        {   // ex4
-            using node = cxjson::node;
+        std::string s1; // write
+            cxon::to_chars(s1, n0);
+        CHECK(s1 == s0);
+    }
+    {   // ex4
+        using node = cxjson::node;
 
-            // build using initializer lists
-            node n1 = node::object {
-                { "object", node::object { {"object", 0} } },
-                { "array", node::array {
-                        node::object { {"object", 0} }, // objects and
-                        node::array { 1, 2, 3 },        // arrays must be explicit
-                        "4",        // string
-                        5,          // number
-                        true,       // boolean
-                        nullptr     // null
-                    }
-                },
-                { "string", "string" }, // "key": value
-                { "number", 3.14 },
-                { "boolean", false },
-                { "null", nullptr }
-            };
+        // build using initializer lists
+        node n1 = node::object {
+            { "object", node::object { {"object", 0} } },
+            { "array", node::array {
+                    node::object { {"object", 0} }, // objects and
+                    node::array { 1, 2, 3 },        // arrays must be explicit
+                    "4",        // string
+                    5,          // number
+                    true,       // boolean
+                    nullptr     // null
+                }
+            },
+            { "string", "string" }, // "key": value
+            { "number", 3.14 },
+            { "boolean", false },
+            { "null", nullptr }
+        };
 
-            // build using node's methods
-            node n2;
-                CHECK(n2.is<node::null>()); // default node type is node_type::null
-                auto& o = n2.imbue<node::object>(); // change the type and return its value
-                    CHECK(n2.is<node::object>());
-                    o["object"] = node::object {};      CHECK(o["object"].is<node::object>());
-                    o["array"] = node::array {};        CHECK(o["array"].is<node::array>());
-                    o["string"] = "string";             CHECK(o["string"].is<node::string>());
-                    o["number"] = 3.14;                 CHECK(o["number"].is<node::number>());
-                    o["boolean"] = false;               CHECK(o["boolean"].is<node::boolean>());
-                    o["null"] = nullptr;                CHECK(o["null"].is<node::null>());
-                auto& o1 = o["object"].get<node::object>(); // get value reference, the type is known
-                    o1["object"] = 0;
-                auto& a = o["array"].get<node::array>();    // get value reference, the type is known
-                    a.push_back(node::object {});       CHECK(a.back().is<node::object>());
-                    a.push_back(node::array {1, 2, 3}); CHECK(a.back().is<node::array>());
-                    a.push_back("4");                   CHECK(a.back().is<node::string>());
-                    a.push_back(5);                     CHECK(a.back().is<node::number>());
-                    a.push_back(true);                  CHECK(a.back().is<node::boolean>());
-                    a.push_back(nullptr);               CHECK(a.back().is<node::null>());
-                auto* o2 = a[0].get_if<node::object>(); // get value pointer if the type match
-                    (*o2)["object"] = 0;
-            CHECK(n1 == n2);
+        // build using node's methods
+        node n2;
+            CHECK(n2.is<node::null>()); // default node type is node_type::null
+            auto& o = n2.imbue<node::object>(); // change the type and return its value
+                CHECK(n2.is<node::object>());
+                o["object"] = node::object {};      CHECK(o["object"].is<node::object>());
+                o["array"] = node::array {};        CHECK(o["array"].is<node::array>());
+                o["string"] = "string";             CHECK(o["string"].is<node::string>());
+                o["number"] = 3.14;                 CHECK(o["number"].is<node::number>());
+                o["boolean"] = false;               CHECK(o["boolean"].is<node::boolean>());
+                o["null"] = nullptr;                CHECK(o["null"].is<node::null>());
+            auto& o1 = o["object"].get<node::object>(); // get value reference, the type is known
+                o1["object"] = 0;
+            auto& a = o["array"].get<node::array>();    // get value reference, the type is known
+                a.push_back(node::object {});       CHECK(a.back().is<node::object>());
+                a.push_back(node::array {1, 2, 3}); CHECK(a.back().is<node::array>());
+                a.push_back("4");                   CHECK(a.back().is<node::string>());
+                a.push_back(5);                     CHECK(a.back().is<node::number>());
+                a.push_back(true);                  CHECK(a.back().is<node::boolean>());
+                a.push_back(nullptr);               CHECK(a.back().is<node::null>());
+            auto* o2 = a[0].get_if<node::object>(); // get value pointer if the type match
+                (*o2)["object"] = 0;
+        CHECK(n1 == n2);
 
-            std::string s1;
-                cxon::to_chars(s1, n1);
-            std::string s2;
-                cxon::to_chars(s2, n2);
-            CHECK(s1 == s2);
+        std::string s1;
+            cxon::to_chars(s1, n1);
+        std::string s2;
+            cxon::to_chars(s2, n2);
+        CHECK(s1 == s2);
 
-            std::string const pretty_json = cxon::pretty(s1);
+        std::string const pretty_json = cxon::pretty(s1);
+    }
+    {   // ex5
+        using node = cxjson::node;
+        {   // (1)
+            node n; CHECK(n.is<node::null>());
         }
-        {   // ex5
-            using node = cxjson::node;
-            {   // (1)
-                node n; CHECK(n.is<node::null>());
-            }
-            {   // (2)
-                node o = true; CHECK(o.is<node::boolean>());
-                node n(o); CHECK(n.is<node::boolean>() && n.get<node::boolean>());
-            }
-            {   // (3)
-                node n(42.0); CHECK(n.is<node::number>() && n.get<node::number>() == 42.0);
-            }
-            {   // (3)
-                node::object const o = { {"key", "value"} };
-                node n(o); CHECK(n.is<node::object>() && n.get<node::object>() == o);
-            }
-            {   // (3)
-                node::array const a = { 1, "string" };
-                node n(a); CHECK(n.is<node::array>() && n.get<node::array>() == a);
-            }
-            {   // (4)
-                node n(42); CHECK(n.is<node::number>() && n.get<node::number>() == 42);
-            }
-            {   // (4)
-                node n("string"); CHECK(n.is<node::string>() && n.get<node::string>() == "string");
-            }
+        {   // (2)
+            node o = true; CHECK(o.is<node::boolean>());
+            node n(o); CHECK(n.is<node::boolean>() && n.get<node::boolean>());
         }
-        {   // ex6
-            {   // T is the same
-                cxjson::node n = "string";
-                cxjson::node::string& v = n.imbue<cxjson::node::string>(); CHECK(v == "string");
-            }
-            {   // T is not the same
-                cxjson::node n = "string";
-                cxjson::node::number& v = n.imbue<cxjson::node::number>(); CHECK(v == 0.0);
-            }
+        {   // (3)
+            node n(42.0); CHECK(n.is<node::number>() && n.get<node::number>() == 42.0);
         }
-        {   // ex7
-            cxjson::node n = "one";
-                n.get<cxjson::node::string>() = "another";
-            CHECK(n.get<cxjson::node::string>() == "another");
+        {   // (3)
+            node::object const o = { {"key", "value"} };
+            node n(o); CHECK(n.is<node::object>() && n.get<node::object>() == o);
         }
-        {   // ex8
-            cxjson::node n = "one";
-                auto *v = n.get_if<cxjson::node::string>(); CHECK(v != nullptr);
-            CHECK(n.get_if<cxjson::node::array>() == nullptr);
+        {   // (3)
+            node::array const a = { 1, "string" };
+            node n(a); CHECK(n.is<node::array>() && n.get<node::array>() == a);
         }
-        {   // ex9
-            cxjson::node const n; CHECK(n.type() == cxjson::node_type::null);
+        {   // (4)
+            node n(42); CHECK(n.is<node::number>() && n.get<node::number>() == 42);
         }
-        {   // ex10
-            {   node a = nullptr, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = true, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = 42, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = "blah", b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = node::array {4, 2}, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = node::object {{"q", "?"}, {"a", 42}}, b;
-                b = a; CHECK(a == b);
-            }
+        {   // (4)
+            node n("string"); CHECK(n.is<node::string>() && n.get<node::string>() == "string");
         }
-        {   // ex11
-            using node = cxjson::node;
-            {   node a = nullptr, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = true, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = 42, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = "blah", b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = node::array {4, 2}, b;
-                b = a; CHECK(a == b);
-            }
-            {   node a = node::object {{"q", "?"}, {"a", 42}}, b;
-                b = a; CHECK(a == b);
-            }
+    }
+    {   // ex6
+        {   // T is the same
+            cxjson::node n = "string";
+            cxjson::node::string& v = n.imbue<cxjson::node::string>(); CHECK(v == "string");
+        }
+        {   // T is not the same
+            cxjson::node n = "string";
+            cxjson::node::number& v = n.imbue<cxjson::node::number>(); CHECK(v == 0.0);
+        }
+    }
+    {   // ex7
+        cxjson::node n = "one";
+            n.get<cxjson::node::string>() = "another";
+        CHECK(n.get<cxjson::node::string>() == "another");
+    }
+    {   // ex8
+        cxjson::node n = "one";
+            auto *v = n.get_if<cxjson::node::string>(); CHECK(v != nullptr);
+        CHECK(n.get_if<cxjson::node::array>() == nullptr);
+    }
+    {   // ex9
+        cxjson::node const n; CHECK(n.type() == cxjson::node_type::null);
+    }
+    {   // ex10
+        {   node a = nullptr, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = true, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = 42, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = "blah", b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::array {4, 2}, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::object {{"q", "?"}, {"a", 42}}, b;
+            b = a; CHECK(a == b);
+        }
+    }
+    {   // ex11
+        using node = cxjson::node;
+        {   node a = nullptr, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = true, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = 42, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = "blah", b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::array {4, 2}, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::object {{"q", "?"}, {"a", 42}}, b;
+            b = a; CHECK(a == b);
         }
     }
 #   undef CHECK
@@ -455,47 +462,67 @@ int main(int argc, char *argv[]) {
         ;
     }
     if (!diff.empty()) {
+        static auto const name = [](const std::string& p) {
+            auto f = p.find_last_of("/\\"); f = f != p.npos ? f + 1 : 0;
+            auto l = p.rfind('.'); if (l == p.npos) l = p.size();
+            return std::string(p, f, l - f);
+        };
         for (auto& c : diff) {
             std::string json;
-            {
+            {   // read
                 std::ifstream is(c.source, std::ifstream::binary);
                     if (!is) {
-                        c.error = "cannot be opened";
+                        ++err, c.error = "cannot be opened";
                         continue;
                     }
                 json.assign(std::istreambuf_iterator<char>(is), std::istreambuf_iterator<char>());
             }
-            static auto const name = [](const std::string& p) {
-                auto f = p.find_last_of("/\\"); f = f != p.npos ? f + 1 : 0;
-                auto l = p.rfind('.'); if (l == p.npos) l = p.size();
-                return std::string(p, f, l - f);
-            };
-            std::ofstream os(name(c.source) + ".0.json", std::ofstream::binary);
-                if (!os) {
-                    c.error = name(c.source) + ".0.json" + ": cannot be opened";
-                    continue;
-                }
-            auto o = cxon::make_indenter(std::ostreambuf_iterator<char>(os));
-            for (auto c : json)  *o = c;
             node result;
-            if (auto const r = cxon::from_chars(result, json)) {
-                std::ofstream os(name(c.source) + ".1.json", std::ofstream::binary);
+            {   // from
+                std::ofstream os(name(c.source) + ".0.json", std::ofstream::binary);
                     if (!os) {
-                        c.error = name(c.source) + ".1.json" + "cannot be opened";
+                        ++err, c.error = name(c.source) + ".0.json" + ": cannot be opened";
                         continue;
                     }
                 auto o = cxon::make_indenter(std::ostreambuf_iterator<char>(os));
-                auto const w = cxon::to_chars(o, result); CXON_ASSERT(w, "unexpected");
-                fprintf(stdout, "%s %s ", (name(c.source) + ".0.json").c_str(), (name(c.source) + ".1.json").c_str());
+                    for (auto c : json)  *o = c;
+                auto const r = cxon::from_chars(result, json);
+                    if (!r) {
+                        ++err, c.error = format_error(r, json.cbegin());
+                        continue;
+                    }
             }
-            else {
-                c.error = format_error(r, json.cbegin());
+            {   // to
+                std::ofstream os(name(c.source) + ".1.json", std::ofstream::binary);
+                    if (!os) {
+                        ++err, c.error = name(c.source) + ".1.json" + "cannot be opened";
+                        continue;
+                    }
+                auto o = cxon::make_indenter(std::ostreambuf_iterator<char>(os));
+                auto const w = cxon::to_chars(o, result);
+                    if (!w) {
+                        //++err, c.error = format_error(w, o);
+                        ++err, c.error += w.ec.category().name(),
+                        c.error += ": " + w.ec.message();
+                        continue;
+                        CXON_ASSERT(w, "unexpected");
+                        fprintf(stdout, "%s %s ", (name(c.source) + ".0.json").c_str(), (name(c.source) + ".1.json").c_str());
+                    }
             }
         }
-        for (auto& c : diff) {
-            if (!c.error.empty()) {
-                fprintf(stderr, "%s:\n\tfailed: %s\n", c.source.c_str(), c.error.c_str());
+        size_t fc = 0;
+            for (auto& c : diff) {
+                if (!c.error.empty()) {
+                    ++fc, fprintf(stderr, "%s:\n\tfailed: %s\n", c.source.c_str(), c.error.c_str());
+                }
             }
+        if (!fc) {
+            for (auto& c : diff) {
+                fprintf(stdout, "%s %s ", (name(c.source) + ".0.json").c_str(), (name(c.source) + ".1.json").c_str());
+            }
+        }
+        else {
+            fprintf(stdout, "cxjson/diff: %zu of %zu failed\n", fc, diff.size());
         }
     }
     if (!time.empty()) {
