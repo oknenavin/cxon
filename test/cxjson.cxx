cdd0845bb636f92a9db30bc8b838208eddba376a
Nikolai Ivanenko <nivanenko@outlook.com>
2019-05-25 14:54:03 +0300

    cxon: generic context parameters - cxjson::recursion_depth test updated (handle apparent g++ bug)

--- test/cxjson.cxx
+++ test/cxjson.cxx
@@ -188,9 +188,15 @@ static unsigned self() {
             CHECK(s0 == s1);
         }
         {   node jn;
-            auto const r = cxon::from_chars(jn, "[[[[", cxjson::recursion_depth::set<unsigned, 4U>());
+            auto const r = cxon::from_chars(jn, "[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[");
             CHECK(!r && r.ec == cxjson::error::recursion_depth_exceeded);
         }
+#       if !defined(__GNUG__) || defined(__clang__)
+        {   node jn;
+            auto const r = cxon::from_chars(jn, "[[[[", cxjson::recursion_depth::set<unsigned, 4U>()); // g++ (4.8.1->9.1) bug: overload resolution fail
+            CHECK(!r && r.ec == cxjson::error::recursion_depth_exceeded);
+        }
+#       endif
         {   node jn;
             auto const r = cxon::from_chars(jn, "~");
             CHECK(!r && r.ec == cxjson::error::invalid);
