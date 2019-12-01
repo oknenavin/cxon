31a1922c781b33fa8dbc7eb4b784f77632ae041d
oknenavin <oknenavin@outlook.com>
2019-12-01 22:51:05 +0200

    cxon: charconv: support for integral bases

--- src/cxon/bits/charconv.hxx
+++ src/cxon/bits/charconv.hxx
@@ -10,6 +10,7 @@
 #include <cstdio>
 #include <cerrno>
 
+#include <algorithm> // reverse
 #include <limits>
 #include <type_traits>
 
@@ -68,7 +69,7 @@ namespace cxon { namespace bits { namespace charconv {
 
         template <typename T, typename Cv>
             inline from_chars_result number_from_chars(const char* first, const char*, Cv convert, T& value, int base) {
-                CXON_ASSERT(base == 2 || base == 8 || base == 10 || base == 16, "unsupported base");
+                CXON_ASSERT(base >= 2 && base <= 36, "invalid base");
                     if (is_sign_invalid<T>(*first)) return { first, errc::invalid_argument };
                 char* end;
                 errno = 0; auto const v = clamp<T>(convert(first, &end, base));
@@ -116,34 +117,43 @@ namespace cxon { namespace bits { namespace charconv {
 
     namespace {
 
-        template <typename> struct fmt;
-        template <> struct fmt<char>                // %c writes a single character but, the argument is first converted to unsigned char
-                                                    { static constexpr char const*const str = "%hhu";   }; // no char[], clang complains ...
-        template <> struct fmt<signed char>         { static constexpr char const*const str = "%hhi";   };
-        template <> struct fmt<unsigned char>       { static constexpr char const*const str = "%hhu";   };
-        template <> struct fmt<short>               { static constexpr char const*const str = "%hi";    };
-        template <> struct fmt<unsigned short>      { static constexpr char const*const str = "%hu";    };
-        template <> struct fmt<int>                 { static constexpr char const*const str = "%i";     };
-        template <> struct fmt<unsigned int>        { static constexpr char const*const str = "%u";     };
-        template <> struct fmt<long>                { static constexpr char const*const str = "%li";    };
-        template <> struct fmt<unsigned long>       { static constexpr char const*const str = "%lu";    };
-        template <> struct fmt<long long>           { static constexpr char const*const str = "%lli";   };
-        template <> struct fmt<unsigned long long>  { static constexpr char const*const str = "%llu";   };
-        template <> struct fmt<float>               { static constexpr char const*const str = "%.*g";   };
-        template <> struct fmt<double>              { static constexpr char const*const str = "%.*g";   };
-        template <> struct fmt<long double>         { static constexpr char const*const str = "%.*Lg";  };
+        template <typename T>
+            inline auto itoa(char* b, T t, int base) -> typename std::enable_if<std::is_unsigned<T>::value, unsigned>::type {
+                char    *p = b;
+                do      *p = "0123456789abcdefghijklmnopqrstuvwxyz"[t % base], ++p;
+                while   (t /= base);
+                return  std::reverse(b, p), unsigned(p - b);
+            }
+        template <typename T>
+            inline auto itoa(char* b, T t, int base) -> typename std::enable_if<std::is_signed<T>::value, unsigned>::type {
+                return t < 0 ?
+                    *b = '-', itoa<typename std::make_unsigned<T>::type>(++b, -t, base) + 1 :
+                    itoa<typename std::make_unsigned<T>::type>(b, t, base)
+                ;
+            }
 
         // to_chars: on error, returns a value of type to_chars_result holding errc::value_too_large in ec,
         //  a copy of the value last in ptr, and leaves the contents of the range [first, last) in unspecified state,
         //  but we don't care, the buffer must be sufficient
         template <typename T>
-            inline to_chars_result number_to_chars(char* first, char* last, T value) {
-                size_t const l = last - first;
-                int const w = snprintf(first, l, fmt<T>::str, value); CXON_ASSERT(w > 0 && (size_t)w < l, "conversion failed");
+            inline auto number_to_chars(char* first, char* last, T value, int base)
+                 -> typename std::enable_if<is_integral<T>::value, to_chars_result>::type
+            {
+                CXON_ASSERT(base >= 2 && base <= 36, "invalid base");
+                CXON_ASSERT(last > first && size_t(last - first) >= std::numeric_limits<T>::digits10 + 1, "buffer too small");
+                unsigned const w = itoa(first, value, base);
                 return { first + w, errc{} };
             }
+
+        template <typename> struct fmt;
+        template <> struct fmt<float>       { static constexpr char const*const str = "%.*g";   };
+        template <> struct fmt<double>      { static constexpr char const*const str = "%.*g";   };
+        template <> struct fmt<long double> { static constexpr char const*const str = "%.*Lg";  };
+
         template <typename T>
-            inline to_chars_result number_to_chars(char* first, char* last, T value, int precision) {
+            inline auto number_to_chars(char* first, char* last, T value, int precision)
+                 -> typename std::enable_if<is_floating_point<T>::value, to_chars_result>::type
+            {
                 size_t const l = last - first;
                 int const w = snprintf(first, l, fmt<T>::str, precision, value); CXON_ASSERT(w > 0 && (size_t)w < l, "conversion failed");
                 return { first + w, errc{} };
@@ -152,10 +162,9 @@ namespace cxon { namespace bits { namespace charconv {
     }
 
 #   define CXON_TO_CHARS(T)\
-        inline to_chars_result to_chars(char* first, char* last, T value) {\
-            return number_to_chars<T>(first, last, value);\
+        inline to_chars_result to_chars(char* first, char* last, T value, int base = 10) {\
+            return number_to_chars<T>(first, last, value, base);\
         }
-        CXON_TO_CHARS(char)
         CXON_TO_CHARS(signed char)
         CXON_TO_CHARS(unsigned char)
         CXON_TO_CHARS(short)
@@ -187,53 +196,66 @@ namespace cxon { namespace bits { // <charconv>
             template <typename T>\
                 struct has_##name<T, enable_if_t<std::is_same<R, decltype(F)>::value>> : std::true_type {}
 
-        HAS_FUNC_DEF(from_chars_i, std::from_chars_result, std::from_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T&>(), 0));
+        HAS_FUNC_DEF(from_chars_b, std::from_chars_result, std::from_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T&>(), 0));
         template <typename T>
-            inline auto from_chars(const char* f, const char* l, T& t, int base) noexcept   -> enable_if_t< has_from_chars_i<T>::value, std::from_chars_result> {
+            inline auto from_chars(const char* f, const char* l, T& t, int base = 10) noexcept
+                -> enable_if_t<std::is_integral<T>::value &&  has_from_chars_b<T>::value, std::from_chars_result>
+            {
                 return std::from_chars(f, l, t, base);
             }
         template <typename T>
-            inline auto from_chars(const char* f, const char* l, T& t, int base)            -> enable_if_t<!has_from_chars_i<T>::value, std::from_chars_result> {
+            inline auto from_chars(const char* f, const char* l, T& t, int base = 10)
+                -> enable_if_t<std::is_integral<T>::value && !has_from_chars_b<T>::value, std::from_chars_result>
+            {
                 auto const r = charconv::from_chars(f, l, t, base);
                 return { r.ptr, r.ec };
             }
 
         HAS_FUNC_DEF(from_chars_f, std::from_chars_result, std::from_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T&>()));
         template <typename T>
-            inline auto from_chars(const char* f, const char* l, T& t) noexcept -> enable_if_t< has_from_chars_f<T>::value, std::from_chars_result> {
+            inline auto from_chars(const char* f, const char* l, T& t) noexcept
+                -> enable_if_t<std::is_floating_point<T>::value &&  has_from_chars_f<T>::value, std::from_chars_result>
+            {
                 return std::from_chars(f, l, t);
             }
         template <typename T>
-            inline auto from_chars(const char* f, const char* l, T& t)          -> enable_if_t<!has_from_chars_f<T>::value, std::from_chars_result> {
+            inline auto from_chars(const char* f, const char* l, T& t)
+                -> enable_if_t<std::is_floating_point<T>::value && !has_from_chars_f<T>::value, std::from_chars_result>
+            {
                 auto const r = charconv::from_chars(f, l, t);
                 return { r.ptr, r.ec };
             }
 
-        HAS_FUNC_DEF(to_chars, std::to_chars_result, std::to_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T>()));
+        HAS_FUNC_DEF(to_chars, std::to_chars_result, std::to_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T>(), 0));
         HAS_FUNC_DEF(to_chars_p, std::to_chars_result, std::to_chars(std::declval<char*>(), std::declval<char*>(), std::declval<T>(), (std::chars_format)0, 0));
 
         template <typename T>
-            inline auto to_chars(char* f, char* l, T t) noexcept    -> enable_if_t< has_to_chars<T>::value, std::to_chars_result> {
-                return std::to_chars(f, l, t);
+            inline auto to_chars(char* f, char* l, T t, int base = 10) noexcept
+                -> enable_if_t<std::is_integral<T>::value &&  has_to_chars<T>::value, std::to_chars_result>
+            {
+                return std::to_chars(f, l, t, base);
             }
         template <typename T>
-            inline auto to_chars(char* f, char* l, T t)             -> enable_if_t<!has_to_chars<T>::value, std::to_chars_result> {
-                auto const r = charconv::to_chars(f, l, t);
+            inline auto to_chars(char* f, char* l, T t, int base = 10)
+                -> enable_if_t<std::is_integral<T>::value && !has_to_chars<T>::value, std::to_chars_result>
+            {
+                auto const r = charconv::to_chars(f, l, t, base);
                 return { r.ptr, r.ec };
             }
 
-        template <typename, typename = void>
+        // gc++: error: 'general' is not a member of 'std::chars_format'
+        template <typename T, typename = void>
             struct general {
-                static constexpr int    value = 7;
+                static constexpr T      value = (T)7;
             };
         template <typename T>
-            struct general<T, enable_if_t<sizeof(T::general) != 0>> {
+            struct general<T, enable_if_t<sizeof(T::general)>> {
                 static constexpr T      value = T::general;
             };
 
         template <typename T>
             inline auto to_chars(char* f, char* l, T t, int precision) noexcept
-                -> enable_if_t< has_to_chars_p<T>::value, std::to_chars_result>
+                -> enable_if_t<std::is_floating_point<T>::value &&  has_to_chars_p<T>::value, std::to_chars_result>
             {
 #               if defined(_MSC_VER) && _MSC_VER <= 1923
                     auto const r = charconv::to_chars(f, l, t, precision);
@@ -244,13 +266,13 @@ namespace cxon { namespace bits { // <charconv>
             }
         template <typename T>
             inline auto to_chars(char* f, char* l, T t, int) noexcept
-                -> enable_if_t<!has_to_chars_p<T>::value &&  has_to_chars<T>::value, std::to_chars_result>
+                -> enable_if_t<std::is_floating_point<T>::value && !has_to_chars_p<T>::value &&  has_to_chars<T>::value, std::to_chars_result>
             {
                 return std::to_chars(f, l, t);
             }
         template <typename T>
             inline auto to_chars(char* f, char* l, T t, int precision)
-                -> enable_if_t<!has_to_chars_p<T>::value && !has_to_chars<T>::value, std::to_chars_result>
+                -> enable_if_t<std::is_floating_point<T>::value && !has_to_chars_p<T>::value && !has_to_chars<T>::value, std::to_chars_result>
             {
                 auto const r = charconv::to_chars(f, l, t, precision);
                 return { r.ptr, r.ec };
