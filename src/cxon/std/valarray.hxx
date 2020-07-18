40fe85b998cadb52f752f192651a34ff98bba738
oknenavin <oknenavin@outlook.com>
2020-07-18 20:24:53 +0300

    update copyright notice

--- src/cxon/std/valarray.hxx
+++ src/cxon/std/valarray.hxx
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2019 oknenavin.
+// Copyright (c) 2017-2020 oknenavin.
 // Licensed under the MIT license. See LICENSE file in the library root for full license information.
 //
 // SPDX-License-Identifier: MIT
