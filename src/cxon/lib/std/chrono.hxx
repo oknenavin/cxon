93f37176ee95d297e7495383f53cf38fc06435b8
oknenavin <oknenavin@outlook.com>
2020-07-26 00:17:47 +0300

    - change directory structure (uncompilable)

---
+++ src/cxon/lib/std/chrono.hxx
@@ -0,0 +1,13 @@
+// Copyright (c) 2017-2020 oknenavin
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef CXON_LIB_STD_CHRONO_
+#   define CXON_LIB_STD_CHRONO_
+
+#   ifdef CXON_JSON_DEFINED_
+#       include "../lang/json/lib/std/chrono.hxx"
+#   endif
+
+#endif // CXON_LIB_STD_CHRONO_
---
+++ src/cxon/lib/std/map.hxx
@@ -0,0 +1,13 @@
+// Copyright (c) 2017-2020 oknenavin
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef CXON_LIB_STD_MAP_
+#   define CXON_LIB_STD_MAP_
+
+#   ifdef CXON_JSON_DEFINED_
+#       include "../lang/json/lib/std/map.hxx"
+#   endif
+
+#endif // CXON_LIB_STD_MAP_
