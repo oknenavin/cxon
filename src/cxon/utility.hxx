9254aec7cd1507619edd36aedd09507d563a4439
oknenavin <oknenavin@outlook.com>
2020-08-27 22:37:07 +0300

    - extract the interface - hide napa details

--- src/cxon/utility.hxx
+++ src/cxon/utility.hxx
@@ -24,10 +24,65 @@
 #   define CXON_ASSERT(e, m) ((void)(e))
 #endif
 
+// interface ///////////////////////////////////////////////////////////////////
+
 namespace cxon {
 
     template <bool C, typename T = void> // C++14
         using enable_if_t = typename std::enable_if<C, T>::type;
+
+    template <typename ...>
+        constexpr bool unexpected();
+
+    template <std::size_t N> struct option;
+
+    // type traits
+
+    template <typename T> struct is_char;
+
+    // iterators
+
+    template <typename I>
+        using iterator_category_t = typename std::iterator_traits<I>::iterator_category;
+
+    template <typename, typename = void>
+        struct is_output_iterator;
+
+    template <typename, typename = void>
+        struct is_forward_iterator;
+
+    template <typename, typename = void>
+        struct is_back_insertable;
+
+    template <typename I>
+        struct continuous;
+
+    template <typename FwIt>
+        struct range_output_iterator;
+
+    template <typename FwIt>
+        constexpr range_output_iterator<FwIt> make_output_iterator(FwIt b, FwIt e);
+
+}
+
+namespace cxon { namespace napa { // named parameters
+
+    template <typename ...Tg>
+        using pack = std::tuple<Tg...>;
+
+    template <typename ...Tg>
+        inline constexpr pack<Tg...> make_pack(Tg&&... t);
+
+    template <typename Tg, typename T, bool E = std::is_scalar<T>::value>
+        struct parameter;
+
+#   define CXON_PARAMETER(P, T) struct P : cxon::napa::parameter<P, T> {}
+
+}}
+
+// implementation //////////////////////////////////////////////////////////////
+
+namespace cxon {
 
     template <typename ...>
         constexpr bool unexpected() { return false; }
@@ -37,31 +92,31 @@ namespace cxon {
 
     // type traits
 
-    template <typename T>   struct is_character : std::false_type {};
-    template <>             struct is_character<char> : std::true_type {};
-    template <>             struct is_character<wchar_t> : std::true_type {};
+    template <typename T>   struct is_char : std::false_type {};
+    template <>             struct is_char<char> : std::true_type {};
+    template <>             struct is_char<wchar_t> : std::true_type {};
 #   if __cplusplus >= 202002L
-        template <>             struct is_character<char8_t> : std::true_type {};
+        template <>             struct is_char<char8_t> : std::true_type {};
 #   endif
-    template <>             struct is_character<char16_t> : std::true_type {};
-    template <>             struct is_character<char32_t> : std::true_type {};
+    template <>             struct is_char<char16_t> : std::true_type {};
+    template <>             struct is_char<char32_t> : std::true_type {};
 
     // iterators
 
     template <typename I>
         using iterator_category_t = typename std::iterator_traits<I>::iterator_category;
 
-    template <typename, typename = void>
+    template <typename, typename/* = void*/>
         struct is_output_iterator : std::false_type {};
     template <typename I>
         struct is_output_iterator<I, enable_if_t<std::is_same<iterator_category_t<I>, std::output_iterator_tag>::value>> : std::true_type {};
 
-    template <typename, typename = void>
+    template <typename, typename/* = void*/>
         struct is_forward_iterator : std::false_type {};
     template <typename I>
         struct is_forward_iterator<I, enable_if_t<!std::is_same<iterator_category_t<I>, std::input_iterator_tag>::value>> : std::true_type {};
 
-    template <typename, typename = void>
+    template <typename, typename/* = void*/>
         struct is_back_insertable : std::false_type {};
     template <typename C>
         struct is_back_insertable<C, decltype(C().push_back(' '))> : std::true_type {};
@@ -96,7 +151,7 @@ namespace cxon {
             }
 
             template <typename T = value_type>
-                auto append(const T* s) -> enable_if_t<is_character<T>::value, range_output_iterator&> {
+                auto append(const T* s) -> enable_if_t<is_char<T>::value, range_output_iterator&> {
                     CXON_ASSERT(*this, "unexpected state");
                     for ( ; b_ != e_ && *s; ++s, ++b_) *b_ = *s;
                     return (g_ = *s == (T)0), *this;
@@ -132,154 +187,151 @@ namespace cxon {
 
 }
 
-namespace cxon { namespace napa { // named parameters
+namespace cxon { namespace napa {
 
-    template <typename ...Tg>
-        using pack = std::tuple<Tg...>;
+    namespace bits {
+
+        template <typename Pk, std::size_t Ix>
+            using tag = typename std::tuple_element<Ix, Pk>::type::tag;
+        template <typename Tg, typename Pk, std::size_t Ix>
+            using is_same_tag = std::is_same<Tg, tag<Pk, Ix>>;
+
+        template <typename Tg>
+            using tag_type = typename Tg::type;
+
+        template <std::size_t Ix, typename T>
+            using tuple_element_t = typename std::tuple_element<Ix, T>::type;
+
+        template <typename Tg, typename Pk, std::size_t Sz = std::tuple_size<Pk>::value>
+            struct has_tag              { static constexpr bool value = !is_same_tag<Tg, Pk, Sz - 1>::value ? has_tag<Tg, Pk, Sz - 1>::value : true; };
+        template <typename Tg, typename Pk>
+            struct has_tag<Tg, Pk, 1>   { static constexpr bool value = is_same_tag<Tg, Pk, 0>::value; };
+        template <typename Tg, typename Pk>
+            struct has_tag<Tg, Pk, 0>   { static constexpr bool value = false; };
+
+        template <typename Tg, typename Pk, std::size_t Sz = std::tuple_size<Pk>::value>
+            struct pack {
+                static constexpr tag_type<Tg>& get(Pk& p)   { return get_(p); }
+                static constexpr tag_type<Tg>  get()        { return get_(); }
+
+                private:
+                    template <typename X = Tg>
+                        static constexpr auto get_(Pk& p) -> enable_if_t< is_same_tag<X, Pk, Sz - 1>::value, tag_type<X>&> {
+                            return std::get<Sz - 1>(p).value;
+                        }
+                    template <typename X = Tg>
+                        static constexpr auto get_(Pk& p) -> enable_if_t<!is_same_tag<X, Pk, Sz - 1>::value, tag_type<X>&> {
+                            return pack<X, Pk, Sz - 1>::get(p);
+                        }
+
+                    template <typename X = Tg>
+                        static constexpr auto get_() -> enable_if_t< is_same_tag<X, Pk, Sz - 1>::value, tag_type<X>> {
+                            return tuple_element_t<Sz - 1, Pk>::value;
+                        }
+                    template <typename X = Tg>
+                        static constexpr auto get_() -> enable_if_t<!is_same_tag<X, Pk, Sz - 1>::value, tag_type<X>> {
+                            return pack<X, Pk, Sz - 1>::get();
+                        }
+            };
+        template <typename Tg, typename Pk>
+            struct pack<Tg, Pk, 1> {
+                static_assert(is_same_tag<Tg, Pk, 0>::value, "no such tag");
+                static constexpr tag_type<Tg>& get(Pk& p)   { return std::get<0>(p).value; }
+                static constexpr tag_type<Tg>  get()        { return tuple_element_t<0, Pk>::value; }
+            };
+        template <typename Tg, typename Pk>
+            struct pack<Tg, Pk, 0> {
+                static_assert(unexpected<Tg>(), "no such tag");
+                static void get(Pk&)    {}
+                static void get()       {}
+            };
+      
+        template <typename Tg, typename Pk>
+            inline constexpr auto get(Pk& p) -> tag_type<Tg>& {
+                return pack<Tg, Pk>::get(p);
+            }
+        template <typename Tg, typename Pk>
+            inline constexpr auto get(const Pk& p) -> const tag_type<Tg>& {
+                return pack<Tg, Pk>::get(const_cast<Pk&>(p));
+            }
+
+        template <typename Tg, typename Pk>
+            inline constexpr auto get() -> tag_type<Tg> {
+                return pack<Tg, Pk>::get();
+            }
+
+
+        template <typename Tg, typename T, T v>
+            struct cst {
+                using tag   = Tg;
+                using type  = T;
+
+                static constexpr type value = v;
+            };
+
+        template <typename Tg, typename T>
+            struct var {
+                using tag   = Tg;
+                using type  = T;
+
+                type value;
+
+                var()               : value() {}
+                var(type&& v)       : value(std::move(v)) {}
+                var(const type& v)  : value(v) {}
+            };
+
+    }
 
     template <typename ...Tg>
         inline constexpr pack<Tg...> make_pack(Tg&&... t) {
             return std::make_tuple(std::forward<Tg>(t)...);
         }
 
-    template <typename Pk, std::size_t Ix>
-        using pack_tag_t = typename std::tuple_element<Ix, Pk>::type::tag;
-    template <typename Tg, typename Pk, std::size_t Ix>
-        using is_same_tag = std::is_same<Tg, pack_tag_t<Pk, Ix>>;
-
-    template <typename Tg>
-        using tag_type_t = typename Tg::tag_type;
-
-    template <std::size_t Ix, typename T>
-        using tuple_element_t = typename std::tuple_element<Ix, T>::type;
-
-    template <typename Tg, typename Pk, std::size_t Sz = std::tuple_size<Pk>::value>
-        struct pack_has_tag             { static constexpr bool value = !is_same_tag<Tg, Pk, Sz - 1>::value ? pack_has_tag<Tg, Pk, Sz - 1>::value : true; };
-    template <typename Tg, typename Pk>
-        struct pack_has_tag<Tg, Pk, 1>  { static constexpr bool value = is_same_tag<Tg, Pk, 0>::value; };
-    template <typename Tg, typename Pk>
-        struct pack_has_tag<Tg, Pk, 0>  { static constexpr bool value = false; };
-
-    template <typename Tg, typename Pk, std::size_t Sz = std::tuple_size<Pk>::value>
-        struct pack_ {
-            static constexpr tag_type_t<Tg>& get(Pk& p) { return get_(p); }
-            static constexpr tag_type_t<Tg>  get()      { return get_(); }
-
-            private:
-                template <typename X = Tg>
-                    static constexpr auto get_(Pk& p) -> enable_if_t< is_same_tag<X, Pk, Sz - 1>::value, tag_type_t<X>&> {
-                        return std::get<Sz - 1>(p).value;
-                    }
-                template <typename X = Tg>
-                    static constexpr auto get_(Pk& p) -> enable_if_t<!is_same_tag<X, Pk, Sz - 1>::value, tag_type_t<X>&> {
-                        return pack_<X, Pk, Sz - 1>::get(p);
-                    }
-
-                template <typename X = Tg>
-                    static constexpr auto get_() -> enable_if_t< is_same_tag<X, Pk, Sz - 1>::value, tag_type_t<X>> {
-                        return tuple_element_t<Sz - 1, Pk>::value;
-                    }
-                template <typename X = Tg>
-                    static constexpr auto get_() -> enable_if_t<!is_same_tag<X, Pk, Sz - 1>::value, tag_type_t<X>> {
-                        return pack_<X, Pk, Sz - 1>::get();
-                    }
-        };
-    template <typename Tg, typename Pk>
-        struct pack_<Tg, Pk, 1> {
-            static_assert(is_same_tag<Tg, Pk, 0>::value, "no such tag");
-
-            static constexpr tag_type_t<Tg>& get(Pk& p) { return std::get<0>(p).value; }
-            static constexpr tag_type_t<Tg>  get()      { return tuple_element_t<0, Pk>::value; }
-        };
-    template <typename Tg, typename Pk>
-        struct pack_<Tg, Pk, 0> {
-            static_assert(unexpected<Tg>(), "no such tag");
-
-            static void get(Pk&)    {}
-            static void get()       {}
-        };
-      
-    template <typename Tg, typename Pk>
-        inline constexpr auto pack_get(Pk& p) -> tag_type_t<Tg>& {
-            return pack_<Tg, Pk>::get(p);
-        }
-    template <typename Tg, typename Pk>
-        inline constexpr auto pack_get(const Pk& p) -> const tag_type_t<Tg>& {
-            return pack_<Tg, Pk>::get(const_cast<Pk&>(p));
-        }
-
-    template <typename Tg, typename Pk>
-        inline constexpr auto pack_get() -> tag_type_t<Tg> {
-            return pack_<Tg, Pk>::get();
-        }
-
-
-    template <typename Tg, typename Tp, Tp v>
-        struct cst {
-            using tag  = Tg;
-            using tag_type  = Tp;
-
-            static constexpr tag_type value = v;
-        };
-
-    template <typename Tg, typename Tp>
-        struct var {
-            using tag  = Tg;
-            using tag_type  = Tp;
-
-            tag_type value;
-
-            var()                   : value() {}
-            var(tag_type&& v)       : value(std::move(v)) {}
-            var(const tag_type& v)  : value(v) {}
-        };
-
-    template <typename Tg, typename Tp, bool E = std::is_scalar<Tp>::value>
+    template <typename Tg, typename T, bool E/* = std::is_scalar<T>::value*/>
         struct parameter {
-            using tag = Tg;
-            using tag_type = Tp;
+            using tag   = Tg;
+            using type  = T;
 
             template <typename Pk>
-                using in = pack_has_tag<Tg, Pk>;
+                using in = bits::has_tag<Tg, Pk>;
 
-            static constexpr var<Tg, Tp> set(Tp&& v)        { return var<Tg, Tp>(std::forward<Tp>(v)); }
-            static constexpr var<Tg, Tp> set(const Tp& v)   { return var<Tg, Tp>(v); }
+            static constexpr bits::var<Tg, T> set(T&& v)        { return bits::var<Tg, T>(std::forward<T>(v)); }
+            static constexpr bits::var<Tg, T> set(const T& v)   { return bits::var<Tg, T>(v); }
 
             template <typename Pk>
-                static constexpr auto constant(Tp)
-                    -> enable_if_t< pack_has_tag<Tg, Pk>::value, Tp>
-                { return pack_get<Tg, Pk>(); }
+                static constexpr auto constant(T)
+                    -> enable_if_t< bits::has_tag<Tg, Pk>::value, T>
+                { return bits::get<Tg, Pk>(); }
             template <typename Pk>
-                static constexpr auto constant(Tp dflt)
-                    -> enable_if_t<!pack_has_tag<Tg, Pk>::value, Tp>
+                static constexpr auto constant(T dflt)
+                    -> enable_if_t<!bits::has_tag<Tg, Pk>::value, T>
                 { return dflt; }
 
             template <typename Pk>
-                static constexpr Tp& reference(Pk& p)   { return pack_get<Tg>(p); }
+                static constexpr T& reference(Pk& p)    { return bits::get<Tg>(p); }
             template <typename Pk>
-                static constexpr Tp value(const Pk& p)  { return pack_get<Tg>(p); }
+                static constexpr T value(const Pk& p)   { return bits::get<Tg>(p); }
 
             template <typename Pk>
-                static constexpr auto value(const Pk& p, Tp)
-                    -> enable_if_t< pack_has_tag<Tg, Pk>::value, Tp>
-                { return pack_get<Tg>(p); }
+                static constexpr auto value(const Pk& p, T)
+                    -> enable_if_t< bits::has_tag<Tg, Pk>::value, T>
+                { return bits::get<Tg>(p); }
             template <typename Pk>
-                static constexpr auto value(const Pk&, Tp dflt)
-                    -> enable_if_t<!pack_has_tag<Tg, Pk>::value, Tp>
+                static constexpr auto value(const Pk&, T dflt)
+                    -> enable_if_t<!bits::has_tag<Tg, Pk>::value, T>
                 { return dflt; }
         };
-    template <typename Tg, typename Tp>
-        struct parameter<Tg, Tp, true> : parameter<Tg, Tp, false> {
-            using tag = Tg;
-            using tag_type = Tp;
+    template <typename Tg, typename T>
+        struct parameter<Tg, T, true> : parameter<Tg, T, false> {
+            using tag   = Tg;
+            using type  = T;
 
-            using parameter<Tg, Tp, false>::set;
+            using parameter<Tg, T, false>::set;
 
-            template <Tp c>
-                static constexpr cst<Tg, Tp, c> set() { return {}; }
+            template <T c>
+                static constexpr bits::cst<Tg, T, c> set() { return {}; }
         };
-
-#   define CXON_PARAMETER(P, T) struct P : cxon::napa::parameter<P, T> {}
 
 }}
 
