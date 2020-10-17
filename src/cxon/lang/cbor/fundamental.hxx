2dc6af2470c2fef52548353822811eb06763c876
oknenavin <oknenavin@outlook.com>
2020-10-18 01:43:11 +0300

    - cbor: floating-point read/write (progress)

--- src/cxon/lang/cbor/fundamental.hxx
+++ src/cxon/lang/cbor/fundamental.hxx
@@ -52,7 +52,7 @@ namespace cxon { // numeric
     namespace bits {
 
         template <typename X, typename T, typename II>
-            inline auto get(T& t, unsigned n, II& i, II e, bio::byte m)
+            inline auto get_(T& t, unsigned n, II& i, II e, bio::byte m)
                 -> enable_if_t<std::is_integral<T>::value, bool>
             {
                 return bio::get(t, n, i, e) && (!(m & X::nint) || (t = -1 - t, true));
@@ -72,15 +72,35 @@ namespace cxon { // numeric
                 case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39:
                 case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47:
                 case 48: case 49: case 50: case 51: case 52: case 53: case 54: case 55:
-                    return t = T((b & X::nint) ? -1 - (b - X::nint) : b), true;
+                    return  t = static_cast<T>((b & X::nint) ? -1 - (b - X::nint) : b), true;
                 case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31:
-                    return bits::get<X>(t, b - 23, i, e, b) || (bio::rewind(i, o), cx|cbor::read_error::integer_invalid);
+                    return  bits::get_<X>(t, b - 23, i, e, b) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::integer_invalid)
+                    ;
                 case 56: case 57: case 58: case 59: case 60: case 61: case 62: case 63:
-                    return bits::get<X>(t, b - 55, i, e, b) || (bio::rewind(i, o), cx|cbor::read_error::integer_invalid);
+                    return  bits::get_<X>(t, b - 55, i, e, b) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::integer_invalid)
+                    ;
                 default:
-                    return                                      bio::rewind(i, o), cx|cbor::read_error::integer_invalid;
+                    return  (bio::rewind(i, o), cx|cbor::read_error::integer_invalid);
             }
         }
+
+    namespace bits {
+
+        template <typename FP>  struct unsigned_;
+        template <>             struct unsigned_<float> { using type = unsigned long; };
+        template <>             struct unsigned_<double> { using type = unsigned long long; };
+        template <typename T>
+            using unsigned_type_ = typename unsigned_<T>::type;
+
+        template <typename FP>  struct signed_;
+        template <>             struct signed_<float> { using type = long; };
+        template <>             struct signed_<double> { using type = long long; };
+        template <typename T>
+            using signed_type_ = typename signed_<T>::type;
+
+    }
 
     template <typename X, typename T, typename II, typename Cx>
         inline auto read_value(T& t, II& i, II e, Cx& cx)
@@ -88,45 +108,79 @@ namespace cxon { // numeric
         {
             II const o = i;
             switch (bio::byte const b = bio::get(i, e)) {
-                /*case  0: case  1: case  2: case  3: case  4: case  5: case  6: case  7:
+                case  0: case  1: case  2: case  3: case  4: case  5: case  6: case  7:
                 case  8: case  9: case 10: case 11: case 12: case 13: case 14: case 15:
                 case 16: case 17: case 18: case 19: case 20: case 21: case 22: case 23:
                 case 32: case 33: case 34: case 35: case 36: case 37: case 38: case 39:
                 case 40: case 41: case 42: case 43: case 44: case 45: case 46: case 47:
                 case 48: case 49: case 50: case 51: case 52: case 53: case 54: case 55:
-                    return t = T((b & X::nint) ? -1 - (b - X::nint) : b), true;
-                case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31:
-                    return bits::get<X>(t, b - 23, i, e, b) || (bio::rewind(i, o), cx|cbor::read_error::integer_invalid);
+                    return  t = static_cast<T>((b & X::nint) ? -1 - (b - X::nint) : b), true;
+                case 24: case 25: case 26: case 27: case 28: case 29: case 30: case 31: {
+                    bits::unsigned_type_<T> tt;
+                    return  (bits::get_<X>(tt, b - 23, i, e, b) && (t = static_cast<T>(tt), true)) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::integer_invalid)
+                    ;
+                }
                 case 56: case 57: case 58: case 59: case 60: case 61: case 62: case 63:
-                    return bits::get<X>(t, b - 55, i, e, b) || (bio::rewind(i, o), cx|cbor::read_error::integer_invalid);*/
+                    bits::signed_type_<T> tt;
+                    return  (bits::get_<X>(tt, b - 55, i, e, b) && (t = static_cast<T>(tt), true)) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::integer_invalid)
+                    ;
                 case X::fp16:
-                    //return bio ::get<2>(t, i, e)            || (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid);
-                    return CXON_ASSERT(0, "not implemented"), false;
+                    /*return  bio ::get<2>(t, i, e) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid)
+                    ;*/
+                    return  CXON_ASSERT(0, "not implemented"), false;
                 case X::fp32:
-                    return bio ::get<4>(t, i, e)            || (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid);
+                    return  bio ::get<4>(t, i, e) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid)
+                    ;
                 case X::fp64:
-                    return bio ::get<8>(t, i, e)            || (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid);
+                    return  bio ::get<8>(t, i, e) ||
+                            (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid)
+                    ;
                 default:
-                    return                                     (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid);
+                    return  (bio::rewind(i, o), cx|cbor::read_error::floating_point_invalid);
             }
         }
 
     namespace bits {
 
         template <typename T>
-            inline auto bytes(T  ) -> enable_if_t<sizeof(T) == 1, unsigned> {
+            inline auto bytes_(T  ) -> enable_if_t<sizeof(T) == 1, unsigned> {
                 return 1;
             }
         template <typename T>
-            inline auto bytes(T t) -> enable_if_t<sizeof(T) == 2, unsigned> {
+            inline auto bytes_(T t) -> enable_if_t<sizeof(T) == 2, unsigned> {
+                /*unsigned r, s;
+                r = (t > 0x00FF) << 3, t >>= r;
+                s = (t > 0x000F) << 2, t >>= s, r |= s;
+                s = (t > 0x0003) << 1, t >>= s, r |= s;
+                                                r |= (t >> 1), r >>= 3, ++r;
+                return r;*/
                 return t >> 8 ? 2 : 1;
             }
         template <typename T>
-            inline auto bytes(T t) -> enable_if_t<sizeof(T) == 4, unsigned> {
+            inline auto bytes_(T t) -> enable_if_t<sizeof(T) == 4, unsigned> {
+                /*unsigned r, s;
+                r = (t > 0xFFFF) << 4, t >>= r;
+                s = (t > 0x00FF) << 3, t >>= s, r |= s;
+                s = (t > 0x000F) << 2, t >>= s, r |= s;
+                s = (t > 0x0003) << 1, t >>= s, r |= s;
+                                                r |= (t >> 1), r >>= 3, ++r;
+                return r;*/
                 return t >> 16 ? (t >> 24 ? 4 : 3) : (t >> 8 ? 2 : 1);
             }
         template <typename T>
-            inline auto bytes(T t) -> enable_if_t<sizeof(T) == 8, unsigned> {
+            inline auto bytes_(T t) -> enable_if_t<sizeof(T) == 8, unsigned> {
+                /*unsigned r, s;
+                r = (t > 0xFFFFFFFF) << 5, t >>= r;
+                s = (t > 0x0000FFFF) << 4, t >>= s, r |= s;
+                s = (t > 0x000000FF) << 3, t >>= s, r |= s;
+                s = (t > 0x0000000F) << 2, t >>= s, r |= s;
+                s = (t > 0x00000003) << 1, t >>= s, r |= s;
+                                                    r |= (t >> 1), r >>= 3, ++r;
+                return r;*/
                 return t >> 32 ? (t >> 48 ? (t >> 56 ? 8 : 7) : (t >> 40 ? 6 : 5)) : (t >> 16 ? (t >> 24 ? 4 : 3) : (t >> 8 ? 2 : 1));
             }
 
@@ -136,7 +190,7 @@ namespace cxon { // numeric
         inline auto write_value(O& o, T t, Cx& cx)
             -> enable_if_t<std::is_integral<T>::value && std::is_unsigned<T>::value && is_same_format<X, CBOR>::value, bool>
         {
-            unsigned const n = bits::bytes(t);
+            unsigned const n = bits::bytes_(t);
             return t > 23 ?
                 (bio::poke(o, bio::byte(X::pint + 23 + n)) && bio::poke(o, t, n))   || (cx|X::write_error::output_failure) :
                  bio::poke(o, bio::byte(X::pint + t))                               || (cx|X::write_error::output_failure)
@@ -147,7 +201,7 @@ namespace cxon { // numeric
             -> enable_if_t<std::is_integral<T>::value && std::is_signed<T>::value && is_same_format<X, CBOR>::value, bool>
         {
             bio::byte const m = t < 0 ? (t = -1 - t, X::nint) : X::pint;
-            unsigned const n = bits::bytes(t);
+            unsigned const n = bits::bytes_(t);
             return t > 23 ?
                 (bio::poke(o, bio::byte(m + 23 + n)) && bio::poke(o, t, n)) || (cx|X::write_error::output_failure) :
                  bio::poke(o, bio::byte(m + t))                             || (cx|X::write_error::output_failure)
