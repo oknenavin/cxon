9bd58a2595b1b64a00a4718f634b7999f90a633a
oknenavin <oknenavin@outlook.com>
2021-02-20 00:04:47 +0200

    - cbor: node implementation (progress)

--- makefile
+++ makefile
@@ -26,6 +26,15 @@ cxon-lang-common-bio = \
     $(srcdir)/lang/common/bio/bio.hxx \
     $(srcdir)/lang/common/bio/io.hxx
 
+cxon-lang-json = \
+    $(srcdir)/lang/json/class.hxx \
+    $(srcdir)/lang/json/compound.hxx \
+    $(srcdir)/lang/json/enum.hxx \
+    $(srcdir)/lang/json/fundamental.hxx \
+    $(srcdir)/lang/json/json.hxx \
+    $(srcdir)/lang/json/json-fwd.hxx \
+    $(srcdir)/lang/json/node.hxx
+
 cxon-lang-json-lib-std-bits = \
     $(srcdir)/lang/json/lib/std/bits/map.hxx \
     $(srcdir)/lang/json/lib/std/bits/set.hxx \
@@ -54,10 +63,23 @@ cxon-lang-json-lib-std = \
     $(srcdir)/lang/json/lib/std/variant.hxx \
     $(srcdir)/lang/json/lib/std/vector.hxx
 
+cxon-lang-json-node = \
+    $(srcdir)/lang/json/node/node.hxx
+
 cxon-lang-cbor-common = \
     $(srcdir)/lang/cbor/common/container.hxx \
     $(srcdir)/lang/cbor/common/sink.hxx \
     $(srcdir)/lang/cbor/common/tag.hxx
+
+cxon-lang-cbor = \
+    $(srcdir)/lang/cbor/cbor.hxx \
+    $(srcdir)/lang/cbor/cbor-fwd.hxx \
+    $(srcdir)/lang/cbor/class.hxx \
+    $(srcdir)/lang/cbor/compound.hxx \
+    $(srcdir)/lang/cbor/enum.hxx \
+    $(srcdir)/lang/cbor/error.hxx \
+    $(srcdir)/lang/cbor/fundamental.hxx \
+    $(srcdir)/lang/cbor/node.hxx
 
 cxon-lang-cbor-lib-std-bits = \
     $(srcdir)/lang/cbor/lib/std/bits/map.hxx \
@@ -86,26 +108,8 @@ cxon-lang-cbor-lib-std = \
     $(srcdir)/lang/cbor/lib/std/variant.hxx \
     $(srcdir)/lang/cbor/lib/std/vector.hxx
 
-cxon-lang-json-node = \
-    $(srcdir)/lang/json/node/node.hxx
-
-cxon-lang-json = \
-    $(srcdir)/lang/json/class.hxx \
-    $(srcdir)/lang/json/compound.hxx \
-    $(srcdir)/lang/json/enum.hxx \
-    $(srcdir)/lang/json/fundamental.hxx \
-    $(srcdir)/lang/json/json.hxx \
-    $(srcdir)/lang/json/json-fwd.hxx \
-    $(srcdir)/lang/json/node.hxx
-
-cxon-lang-cbor = \
-    $(srcdir)/lang/cbor/cbor.hxx \
-    $(srcdir)/lang/cbor/cbor-fwd.hxx \
-    $(srcdir)/lang/cbor/class.hxx \
-    $(srcdir)/lang/cbor/compound.hxx \
-    $(srcdir)/lang/cbor/enum.hxx \
-    $(srcdir)/lang/cbor/error.hxx \
-    $(srcdir)/lang/cbor/fundamental.hxx
+cxon-lang-cbor-node = \
+    $(srcdir)/lang/cbor/node/node.hxx
 
 cxon-lang = \
     $(srcdir)/json.hxx \
@@ -155,31 +159,32 @@ install: install-lang
 
 install-cxon:
 	@install -d                                         $(insdir)/cxon/lib/std
-	@install -p -m 0644 $(cxon-lib-std)                 $(insdir)/cxon/lib/std
 	@install -p -m 0644 $(cxon)                         $(insdir)/cxon
+	@install -p -m 0644 $(cxon-lib-std)                 $(insdir)/cxon/lib/std
 
 install-common: install-cxon
 	@install -d                                         $(insdir)/cxon/lang/common/cio
 	@install -p -m 0644 $(cxon-lang-common-cio)         $(insdir)/cxon/lang/common/cio
 	@install -d                                         $(insdir)/cxon/lang/common/bio
-	@install -p -m 0644 $(cxon-lang-common-bio)         $(insdir)/cxon/lang/common/bio
 	@install -p -m 0644 $(cxon-lang-common)             $(insdir)/cxon/lang/common
+	@install -p -m 0644 $(cxon-lang-common-bio)         $(insdir)/cxon/lang/common/bio
 
 install-lang: install-cxon install-common
+	@install -p -m 0644 $(cxon-lang)                    $(insdir)/cxon
+	@install -d                                         $(insdir)/cxon/lang/json/node
+	@install -p -m 0644 $(cxon-lang-json)               $(insdir)/cxon/lang/json
+	@install -p -m 0644 $(cxon-lang-json-node)          $(insdir)/cxon/lang/json/node
 	@install -d                                         $(insdir)/cxon/lang/json/lib/std/bits
-	@install -p -m 0644 $(cxon-lang-json-lib-std-bits)  $(insdir)/cxon/lang/json/lib/std/bits
 	@install -p -m 0644 $(cxon-lang-json-lib-std)       $(insdir)/cxon/lang/json/lib/std
+	@install -p -m 0644 $(cxon-lang-json-lib-std-bits)  $(insdir)/cxon/lang/json/lib/std/bits
 	@install -d                                         $(insdir)/cxon/lang/cbor/common
 	@install -p -m 0644 $(cxon-lang-cbor-common)        $(insdir)/cxon/lang/cbor/common
-	@install -d                                         $(insdir)/cxon/lang/cbor/lib/std/bits
-	@install -p -m 0644 $(cxon-lang-cbor-lib-std-bits)  $(insdir)/cxon/lang/cbor/lib/std/bits
-	@install -p -m 0644 $(cxon-lang-cbor-lib-std)       $(insdir)/cxon/lang/cbor/lib/std
-	@install -d                                         $(insdir)/cxon/lang/json/node
-	@install -p -m 0644 $(cxon-lang-json-node)          $(insdir)/cxon/lang/json/node
-	@install -p -m 0644 $(cxon-lang-json)               $(insdir)/cxon/lang/json
-	@install -d                                         $(insdir)/cxon/lang/cbor
+	@install -d                                         $(insdir)/cxon/lang/cbor/node
 	@install -p -m 0644 $(cxon-lang-cbor)               $(insdir)/cxon/lang/cbor
-	@install -p -m 0644 $(cxon-lang)                    $(insdir)/cxon
+	@install -p -m 0644 $(cxon-lang-cbor-node)          $(insdir)/cxon/lang/cbor/node
+	@install -d                                         $(insdir)/cxon/lang/cbor/lib/std/bits
+	@install -p -m 0644 $(cxon-lang-cbor-lib-std)       $(insdir)/cxon/lang/cbor/lib/std
+	@install -p -m 0644 $(cxon-lang-cbor-lib-std-bits)  $(insdir)/cxon/lang/cbor/lib/std/bits
 
 uninstall:
 	@rm -fr $(insdir)/cxon
---
+++ test/src/cbor/cbor.node.cxx
@@ -0,0 +1,190 @@
+// Copyright (c) 2017-2021 oknenavin.
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
+#include "cxon/cbor.hxx"
+#include "cxon/lang/cbor/node.hxx"
+
+#include "cxon/lib/std/list.hxx"
+
+////////////////////////////////////////////////////////////////////////////////
+
+//struct my_traits : cxon::cbor::node_traits {
+//    using                               bytes_type = std::vector;
+//    template <class T> using            array_type = std::list<T>;
+//    template <class K, class V> using   map_type = std::multimap<K, V>;
+//};
+
+static unsigned self() {
+    unsigned a_ = 0;
+    unsigned f_ = 0;
+
+#   define CHECK(c) ++a_; if (!(c))\
+        fprintf(stderr, "must pass, but failed: at %s:%li\n", __FILE__, (long)__LINE__), fflush(stderr), ++f_;\
+        CXON_ASSERT((c), "check failed");
+
+    using node = cxon::cbor::node;
+
+    {   // ex4
+        // build using initializer lists
+        node n1 = node::map {
+            { "object", node::map { {"object", 0} } },
+            { "array", node::array {
+                    node::map { {"object", 0} },    // objects and
+                    node::array { 1, 2, 3 },        // arrays must be explicit
+                    //"4",                          // byte_string
+                    "4",                            // text_string
+                    3,                              // signed_integer
+                    14U,                            // unsigned_integer
+                    3.14,                           // floating_point
+                    true,                           // boolean
+                    nullptr                         // null
+                }
+            },
+            { "text_string", "string" }, // "key": value
+            { "signed_integer", 3 },
+            { "unsigned_integer", 14U },
+            { "floating_point", 3.14 },
+            { "boolean", false },
+            { "null", nullptr }
+        };
+
+        // build using node's methods
+        node n2;
+            CHECK(n2.is<node::null>()); // default node type is node_kind::null
+            auto& o = n2.imbue<node::map>(); // change the type and return its value
+                CHECK(n2.is<node::map>());
+                o["object"] = node::map {};         CHECK(o["object"].is<node::map>());
+                o["array"] = node::array {};        CHECK(o["array"].is<node::array>());
+                //o["byte_string"] = "string";        CHECK(o["byte_string"].is<node::byte_string>());
+                o["text_string"] = "string";        CHECK(o["text_string"].is<node::text_string>());
+                o["signed_integer"] = 3;            CHECK(o["signed_integer"].is<node::signed_integer>());
+                o["unsigned_integer"] = 14U;        CHECK(o["unsigned_integer"].is<node::unsigned_integer>());
+                o["floating_point"] = 3.14;         CHECK(o["floating_point"].is<node::floating_point>());
+                o["boolean"] = false;               CHECK(o["boolean"].is<node::boolean>());
+                o["null"] = nullptr;                CHECK(o["null"].is<node::null>());
+            auto& o1 = o["object"].get<node::map>(); // get value reference, the type is known
+                o1["object"] = 0;
+            auto& a = o["array"].get<node::array>();    // get value reference, the type is known
+                a.push_back(node::map {});          CHECK(a.back().is<node::map>());
+                a.push_back(node::array {1, 2, 3}); CHECK(a.back().is<node::array>());
+                a.push_back("4");                   CHECK(a.back().is<node::text_string>());
+                a.push_back(3);                     CHECK(a.back().is<node::signed_integer>());
+                a.push_back(14U);                   CHECK(a.back().is<node::unsigned_integer>());
+                a.push_back(3.14);                  CHECK(a.back().is<node::floating_point>());
+                a.push_back(true);                  CHECK(a.back().is<node::boolean>());
+                a.push_back(nullptr);               CHECK(a.back().is<node::null>());
+            auto* o2 = a[0].get_if<node::map>(); // get value pointer if the type match
+                (*o2)["object"] = 0;
+        CHECK(n1 == n2);
+
+        /*std::string s1;
+            cxon::to_bytes(s1, n1);
+        std::string s2;
+            cxon::to_bytes(s2, n2);
+        CHECK(s1 == s2);*/
+    }
+    {   // ex5
+        {   // (1)
+            node n; CHECK(n.is<node::null>());
+        }
+        {   // (2)
+            node o = true; CHECK(o.is<node::boolean>());
+            node n(o); CHECK(n.is<node::boolean>() && n.get<node::boolean>());
+        }
+        {   // (3)
+            node n(42.0); CHECK(n.is<node::floating_point>() && n.get<node::floating_point>() == 42.0);
+        }
+        {   // (3)
+            node::map const o = { {"key", "value"} };
+            node n(o); CHECK(n.is<node::map>() && n.get<node::map>() == o);
+        }
+        {   // (3)
+            node::array const a = { 1, "string" };
+            node n(a); CHECK(n.is<node::array>() && n.get<node::array>() == a);
+        }
+        {   // (4)
+            node n(42); CHECK(n.is<node::signed_integer>() && n.get<node::signed_integer>() == 42);
+        }
+        {   // (4)
+            node n("string"); CHECK(n.is<node::text_string>() && n.get<node::text_string>() == "string");
+        }
+    }
+    {   // ex6
+        {   // T is the same
+            node n = "string";
+            node::text_string& v = n.imbue<node::text_string>(); CHECK(v == "string");
+        }
+        {   // T is not the same
+            node n = "string";
+            node::floating_point& v = n.imbue<node::floating_point>(); CHECK(v == 0.0);
+        }
+    }
+    {   // ex7
+        node n = "one";
+            n.get<node::text_string>() = "another";
+        CHECK(n.get<node::text_string>() == "another");
+    }
+    {   // ex8
+        node n = "one";
+            auto *v = n.get_if<node::text_string>(); CHECK(v != nullptr);
+        CHECK(n.get_if<node::array>() == nullptr);
+    }
+    {   // ex9
+        node const n; CHECK(n.kind() == cxon::cbor::node_kind::null);
+    }
+    {   // ex10
+        {   node a = nullptr, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = true, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = 42, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = "blah", b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::array {4, 2}, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::map {{"q", "?"}, {"a", 42}}, b;
+            b = a; CHECK(a == b);
+        }
+    }
+    {   // ex11
+        {   node a = nullptr, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = true, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = 42, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = "blah", b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::array {4, 2}, b;
+            b = a; CHECK(a == b);
+        }
+        {   node a = node::map {{"q", "?"}, {"a", 42}}, b;
+            b = a; CHECK(a == b);
+        }
+    }
+#   undef CHECK
+    f_ ?
+        fprintf(stdout, "cxon/cbor/node/self: %u of %u failed\n", f_, a_) :
+        fprintf(stdout, "cxon/cbor/node/self: %u of %u passed\n", a_, a_)
+    ;
+    return f_;
+}
+
+int main(int argc, char *[]) {
+    if (argc == 1) {
+        return self();
+    }
+    return 0;
+}
