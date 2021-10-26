5a16f5f2eaf4ef2b257e8a897dfc71ae3c083763
oknenavin <oknenavin@outlook.com>
2021-10-26 23:34:38 +0300

    - test: separate cbor/node test code

---
+++ test/src/cbor/cbor.node.hxx
@@ -0,0 +1,45 @@
+// Copyright (c) 2017-2021 oknenavin.
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef CXON_TEST_CBOR_NODE_HXX_
+#define CXON_TEST_CBOR_NODE_HXX_
+
+#include "../utility.hxx"
+
+#include <string>
+#include <vector>
+#include <map>
+
+namespace test {
+    
+    struct fix {
+        std::string act;
+        std::string data;
+        std::string fail;
+    };
+    struct fixture {
+        std::string type;
+        std::vector<std::string> in;
+        std::map<std::string, struct fix> fix;
+    };
+
+    struct result {
+        int err = 0;
+        int all = 0;
+    };
+
+}
+
+namespace test { namespace kind {
+
+    int self();
+
+    result vector(const fixture& fixture);
+    result roundtrip(const fixture& fixture);
+    result time(const fixture& fixture);
+
+}}
+
+#endif // CXON_TEST_CBOR_NODE_HXX_
