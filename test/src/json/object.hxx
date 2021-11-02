05eef1a4236042111daaeaddb742e82836b41f87
oknenavin <oknenavin@outlook.com>
2021-11-02 02:44:24 +0200

    - test: complete native json benchmark for all input files

--- test/src/json/object.hxx
+++ test/src/json/object.hxx
@@ -147,7 +147,7 @@ namespace test {
 
     }
 
-    #ifdef CXON_HAS_LIB_STD_OPTIONAL
+#   ifdef CXON_HAS_LIB_STD_OPTIONAL
         namespace citm_catalog {
 
             struct object {
@@ -257,7 +257,416 @@ namespace test {
             };
 
         }
-    #endif
+#   endif
+
+#   ifdef CXON_HAS_LIB_STD_OPTIONAL
+        namespace github_events {
+
+            struct actor {
+                std::string gravatar_id;
+                std::string login;
+                std::string avatar_url;
+                std::string url;
+                unsigned long long id;
+
+                CXON_JSON_CLS_MEMBER(actor,
+                    CXON_JSON_CLS_FIELD_ASIS(gravatar_id),
+                    CXON_JSON_CLS_FIELD_ASIS(login),
+                    CXON_JSON_CLS_FIELD_ASIS(avatar_url),
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(id)
+                )
+            };
+
+            struct repo {
+                std::string url;
+                unsigned long long id;
+                std::string name;
+
+                CXON_JSON_CLS_MEMBER(repo,
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(id),
+                    CXON_JSON_CLS_FIELD_ASIS(name)
+                )
+            };
+
+            struct author {
+                std::string email;
+                std::string name;
+
+                CXON_JSON_CLS_MEMBER(author,
+                    CXON_JSON_CLS_FIELD_ASIS(email),
+                    CXON_JSON_CLS_FIELD_ASIS(name)
+                )
+            };
+
+            struct commit {
+                std::string url;
+                std::string message;
+                bool distinct;
+                std::string sha;
+                struct author author;
+
+                CXON_JSON_CLS_MEMBER(commit,
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(message),
+                    CXON_JSON_CLS_FIELD_ASIS(distinct),
+                    CXON_JSON_CLS_FIELD_ASIS(sha),
+                    CXON_JSON_CLS_FIELD_ASIS(author)
+                )
+            };
+
+            struct user {
+                std::string url;
+                std::string gists_url;
+                std::string gravatar_id;
+                std::string type;
+                std::string avatar_url;
+                std::string subscriptions_url;
+                std::string organizations_url;
+                std::string received_events_url;
+                std::string repos_url;
+                std::string login;
+                unsigned long long id;
+                std::string starred_url;
+                std::string events_url;
+                std::string followers_url;
+                std::string following_url;
+
+                CXON_JSON_CLS_MEMBER(user,
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(gists_url),
+                    CXON_JSON_CLS_FIELD_ASIS(gravatar_id),
+                    CXON_JSON_CLS_FIELD_ASIS(type),
+                    CXON_JSON_CLS_FIELD_ASIS(avatar_url),
+                    CXON_JSON_CLS_FIELD_ASIS(subscriptions_url),
+                    CXON_JSON_CLS_FIELD_ASIS(organizations_url),
+                    CXON_JSON_CLS_FIELD_ASIS(received_events_url),
+                    CXON_JSON_CLS_FIELD_ASIS(repos_url),
+                    CXON_JSON_CLS_FIELD_ASIS(login),
+                    CXON_JSON_CLS_FIELD_ASIS(id),
+                    CXON_JSON_CLS_FIELD_ASIS(starred_url),
+                    CXON_JSON_CLS_FIELD_ASIS(events_url),
+                    CXON_JSON_CLS_FIELD_ASIS(followers_url),
+                    CXON_JSON_CLS_FIELD_ASIS(following_url)
+                )
+            };
+
+            struct forkee {
+                std::string description;
+                bool fork;
+                std::string url;
+                std::string language;
+                std::string stargazers_url;
+                std::string clone_url;
+                std::string tags_url;
+                std::string full_name;
+                std::string merges_url;
+                unsigned forks;
+                bool private_;
+                std::string git_refs_url;
+                std::string archive_url;
+                std::string collaborators_url;
+                struct user owner;
+                std::string languages_url;
+                std::string trees_url;
+                std::string labels_url;
+                std::string html_url;
+                std::string pushed_at;
+                std::string created_at;
+                bool has_issues;
+                std::string forks_url;
+                std::string branches_url;
+                std::string commits_url;
+                std::string notifications_url;
+                unsigned open_issues;
+                std::string contents_url;
+                std::string blobs_url;
+                std::string issues_url;
+                std::string compare_url;
+                std::string issue_events_url;
+                std::string name;
+                std::string updated_at;
+                std::string statuses_url;
+                unsigned forks_count;
+                std::string assignees_url;
+                std::string ssh_url;
+                bool public_;
+                bool has_wiki;
+                std::string subscribers_url;
+                std::optional<std::string> mirror_url;
+                unsigned watchers_count;
+                unsigned long long id;
+                bool has_downloads;
+                std::string git_commits_url;
+                std::string downloads_url;
+                std::string pulls_url;
+                std::optional<std::string> homepage;
+                std::string issue_comment_url;
+                std::string hooks_url;
+                std::string subscription_url;
+                std::string milestones_url;
+                std::string svn_url;
+                std::string events_url;
+                std::string git_tags_url;
+                std::string teams_url;
+                std::string comments_url;
+                unsigned open_issues_count;
+                std::string keys_url;
+                std::string git_url;
+                std::string contributors_url;
+                unsigned size;
+                unsigned watchers;
+
+                CXON_JSON_CLS_MEMBER(forkee,
+                    CXON_JSON_CLS_FIELD_ASIS(description),
+                    CXON_JSON_CLS_FIELD_ASIS(fork),
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(language),
+                    CXON_JSON_CLS_FIELD_ASIS(stargazers_url),
+                    CXON_JSON_CLS_FIELD_ASIS(clone_url),
+                    CXON_JSON_CLS_FIELD_ASIS(tags_url),
+                    CXON_JSON_CLS_FIELD_ASIS(full_name),
+                    CXON_JSON_CLS_FIELD_ASIS(merges_url),
+                    CXON_JSON_CLS_FIELD_ASIS(forks),
+                    CXON_JSON_CLS_FIELD_NAME("private", private_),
+                    CXON_JSON_CLS_FIELD_ASIS(git_refs_url),
+                    CXON_JSON_CLS_FIELD_ASIS(archive_url),
+                    CXON_JSON_CLS_FIELD_ASIS(collaborators_url),
+                    CXON_JSON_CLS_FIELD_ASIS(owner),
+                    CXON_JSON_CLS_FIELD_ASIS(languages_url),
+                    CXON_JSON_CLS_FIELD_ASIS(trees_url),
+                    CXON_JSON_CLS_FIELD_ASIS(labels_url),
+                    CXON_JSON_CLS_FIELD_ASIS(html_url),
+                    CXON_JSON_CLS_FIELD_ASIS(pushed_at),
+                    CXON_JSON_CLS_FIELD_ASIS(created_at),
+                    CXON_JSON_CLS_FIELD_ASIS(has_issues),
+                    CXON_JSON_CLS_FIELD_ASIS(forks_url),
+                    CXON_JSON_CLS_FIELD_ASIS(branches_url),
+                    CXON_JSON_CLS_FIELD_ASIS(commits_url),
+                    CXON_JSON_CLS_FIELD_ASIS(notifications_url),
+                    CXON_JSON_CLS_FIELD_ASIS(open_issues),
+                    CXON_JSON_CLS_FIELD_ASIS(contents_url),
+                    CXON_JSON_CLS_FIELD_ASIS(blobs_url),
+                    CXON_JSON_CLS_FIELD_ASIS(issues_url),
+                    CXON_JSON_CLS_FIELD_ASIS(compare_url),
+                    CXON_JSON_CLS_FIELD_ASIS(issue_events_url),
+                    CXON_JSON_CLS_FIELD_ASIS(name),
+                    CXON_JSON_CLS_FIELD_ASIS(updated_at),
+                    CXON_JSON_CLS_FIELD_ASIS(statuses_url),
+                    CXON_JSON_CLS_FIELD_ASIS(forks_count),
+                    CXON_JSON_CLS_FIELD_ASIS(assignees_url),
+                    CXON_JSON_CLS_FIELD_ASIS(ssh_url),
+                    CXON_JSON_CLS_FIELD_NAME("public", public_),
+                    CXON_JSON_CLS_FIELD_ASIS(has_wiki),
+                    CXON_JSON_CLS_FIELD_ASIS(subscribers_url),
+                    CXON_JSON_CLS_FIELD_ASIS(mirror_url),
+                    CXON_JSON_CLS_FIELD_ASIS(watchers_count),
+                    CXON_JSON_CLS_FIELD_ASIS(id),
+                    CXON_JSON_CLS_FIELD_ASIS(has_downloads),
+                    CXON_JSON_CLS_FIELD_ASIS(git_commits_url),
+                    CXON_JSON_CLS_FIELD_ASIS(downloads_url),
+                    CXON_JSON_CLS_FIELD_ASIS(pulls_url),
+                    CXON_JSON_CLS_FIELD_ASIS(homepage),
+                    CXON_JSON_CLS_FIELD_ASIS(issue_comment_url),
+                    CXON_JSON_CLS_FIELD_ASIS(hooks_url),
+                    CXON_JSON_CLS_FIELD_ASIS(subscription_url),
+                    CXON_JSON_CLS_FIELD_ASIS(milestones_url),
+                    CXON_JSON_CLS_FIELD_ASIS(svn_url),
+                    CXON_JSON_CLS_FIELD_ASIS(events_url),
+                    CXON_JSON_CLS_FIELD_ASIS(git_tags_url),
+                    CXON_JSON_CLS_FIELD_ASIS(teams_url),
+                    CXON_JSON_CLS_FIELD_ASIS(comments_url),
+                    CXON_JSON_CLS_FIELD_ASIS(open_issues_count),
+                    CXON_JSON_CLS_FIELD_ASIS(keys_url),
+                    CXON_JSON_CLS_FIELD_ASIS(git_url),
+                    CXON_JSON_CLS_FIELD_ASIS(contributors_url),
+                    CXON_JSON_CLS_FIELD_ASIS(size),
+                    CXON_JSON_CLS_FIELD_ASIS(watchers)
+                )
+            };
+
+            struct pull_request {
+                std::optional<std::string> html_url;
+                std::optional<std::string> patch_url;
+                std::optional<std::string> diff_url;
+
+                CXON_JSON_CLS_MEMBER(pull_request,
+                    CXON_JSON_CLS_FIELD_ASIS(html_url),
+                    CXON_JSON_CLS_FIELD_ASIS(patch_url),
+                    CXON_JSON_CLS_FIELD_ASIS(diff_url)
+                )
+            };
+
+            struct issue {
+                struct user user;
+                std::string url;
+                std::vector<std::string> labels;
+                std::string html_url;
+                std::string labels_url;
+                struct pull_request pull_request;
+                std::string created_at;
+                std::optional<std::string> closed_at;
+                std::optional<std::string> milestone;
+                std::string title;
+                std::string body;
+                std::string updated_at;
+                unsigned number;
+                std::string state;
+                std::optional<struct user> assignee;
+                unsigned long long id;
+                std::string events_url;
+                std::string comments_url;
+                unsigned comments;
+
+                CXON_JSON_CLS_MEMBER(issue,
+                    CXON_JSON_CLS_FIELD_ASIS(user),
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(labels),
+                    CXON_JSON_CLS_FIELD_ASIS(html_url),
+                    CXON_JSON_CLS_FIELD_ASIS(labels_url),
+                    CXON_JSON_CLS_FIELD_ASIS(pull_request),
+                    CXON_JSON_CLS_FIELD_ASIS(created_at),
+                    CXON_JSON_CLS_FIELD_ASIS(closed_at),
+                    CXON_JSON_CLS_FIELD_ASIS(milestone),
+                    CXON_JSON_CLS_FIELD_ASIS(title),
+                    CXON_JSON_CLS_FIELD_ASIS(body),
+                    CXON_JSON_CLS_FIELD_ASIS(updated_at),
+                    CXON_JSON_CLS_FIELD_ASIS(number),
+                    CXON_JSON_CLS_FIELD_ASIS(state),
+                    CXON_JSON_CLS_FIELD_ASIS(assignee),
+                    CXON_JSON_CLS_FIELD_ASIS(id),
+                    CXON_JSON_CLS_FIELD_ASIS(events_url),
+                    CXON_JSON_CLS_FIELD_ASIS(comments_url),
+                    CXON_JSON_CLS_FIELD_ASIS(comments)
+                )
+            };
+
+            struct comment {
+                struct user user;
+                std::string url;
+                std::string issue_url;
+                std::string created_at;
+                std::string body;
+                std::string updated_at;
+                unsigned long long id;
+
+                CXON_JSON_CLS_MEMBER(comment,
+                    CXON_JSON_CLS_FIELD_ASIS(user),
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(issue_url),
+                    CXON_JSON_CLS_FIELD_ASIS(created_at),
+                    CXON_JSON_CLS_FIELD_ASIS(body),
+                    CXON_JSON_CLS_FIELD_ASIS(updated_at),
+                    CXON_JSON_CLS_FIELD_ASIS(id)
+                )
+            };
+
+            struct page {
+                std::string page_name;
+                std::string html_url;
+                std::string title;
+                std::string sha;
+                std::optional<std::string> summary;
+                std::string action;
+
+                CXON_JSON_CLS_MEMBER(page,
+                    CXON_JSON_CLS_FIELD_ASIS(page_name),
+                    CXON_JSON_CLS_FIELD_ASIS(html_url),
+                    CXON_JSON_CLS_FIELD_ASIS(title),
+                    CXON_JSON_CLS_FIELD_ASIS(sha),
+                    CXON_JSON_CLS_FIELD_ASIS(summary),
+                    CXON_JSON_CLS_FIELD_ASIS(action)
+                )
+            };
+
+            struct payload {
+                // PushEvent
+                std::vector<commit> commits;
+                unsigned distinct_size;
+                std::optional<std::string> ref;
+                unsigned long long push_id;
+                std::string head;
+                std::string before;
+                unsigned size;
+                // CreateEvent
+                std::string description;
+                std::string master_branch;
+                //std::string ref;
+                std::string ref_type;
+                // ForkEvent
+                struct forkee forkee;
+                // WatchEvent
+                std::string action;
+                // IssueCommentEvent
+                struct issue issue;
+                struct comment comment;
+                // IssuesEvent
+                // GollumEvent
+                std::vector<page> pages;
+                // CreateEvent
+
+                CXON_JSON_CLS_MEMBER(payload,
+                    CXON_JSON_CLS_FIELD_ASIS(commits),
+                    CXON_JSON_CLS_FIELD_ASIS(distinct_size),
+                    CXON_JSON_CLS_FIELD_ASIS(ref),
+                    CXON_JSON_CLS_FIELD_ASIS(push_id),
+                    CXON_JSON_CLS_FIELD_ASIS(head),
+                    CXON_JSON_CLS_FIELD_ASIS(before),
+                    CXON_JSON_CLS_FIELD_ASIS(size),
+                    CXON_JSON_CLS_FIELD_ASIS(description),
+                    CXON_JSON_CLS_FIELD_ASIS(master_branch),
+                    CXON_JSON_CLS_FIELD_ASIS(ref_type),
+                    CXON_JSON_CLS_FIELD_ASIS(forkee),
+                    CXON_JSON_CLS_FIELD_ASIS(action),
+                    CXON_JSON_CLS_FIELD_ASIS(issue),
+                    CXON_JSON_CLS_FIELD_ASIS(comment),
+                    CXON_JSON_CLS_FIELD_ASIS(pages)
+                )
+            };
+
+            struct org {
+                std::string gravatar_id;
+                std::string login;
+                std::string avatar_url;
+                std::string url;
+                unsigned long long id;
+
+                CXON_JSON_CLS_MEMBER(org,
+                    CXON_JSON_CLS_FIELD_ASIS(gravatar_id),
+                    CXON_JSON_CLS_FIELD_ASIS(login),
+                    CXON_JSON_CLS_FIELD_ASIS(avatar_url),
+                    CXON_JSON_CLS_FIELD_ASIS(url),
+                    CXON_JSON_CLS_FIELD_ASIS(id)
+                )
+            };
+
+            struct event {
+                std::string type;
+                std::string created_at;
+                struct actor actor;
+                struct repo repo;
+                bool public_;
+                struct payload payload;
+                struct org org;
+                std::string id;
+
+                CXON_JSON_CLS_MEMBER(event,
+                    CXON_JSON_CLS_FIELD_ASIS(type),
+                    CXON_JSON_CLS_FIELD_ASIS(created_at),
+                    CXON_JSON_CLS_FIELD_ASIS(actor),
+                    CXON_JSON_CLS_FIELD_ASIS(repo),
+                    CXON_JSON_CLS_FIELD_NAME("public", public_),
+                    CXON_JSON_CLS_FIELD_ASIS(payload),
+                    CXON_JSON_CLS_FIELD_ASIS(org),
+                    CXON_JSON_CLS_FIELD_ASIS(id)
+                )
+            };
+
+            using object = std::vector<event>;
+
+        }
+#   endif
 
     namespace gsoc_2018 {
 
