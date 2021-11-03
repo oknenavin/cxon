db5ac68b6a2165e7998192f21f068465c4895f49
oknenavin <oknenavin@outlook.com>
2021-11-03 16:18:21 +0200

    - test: more stable benchmark results

--- test/src/utility.hxx
+++ test/src/utility.hxx
@@ -7,12 +7,14 @@
 #define CXON_TEST_UTILITY_HXX_
 
 #include <utility>
-#include <limits>
 #include <chrono>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <numeric>
 #include <system_error>
 
-////////////////////////////////////////////////////////////////////////////////
+// interface ///////////////////////////////////////////////////////////////////
 
 #ifndef CXON_TIME_RUN_FOR
 #   define CXON_TIME_RUN_FOR 250
@@ -21,43 +23,88 @@
 namespace test {
 
     template <typename B>
+        static double measure(B block);
+#   define CXON_MEASURE(...) ::test::measure([&] { __VA_ARGS__ })
+
+    template <typename R, typename II>
+        static std::string format_error(const R& r, II b);
+
+}
+
+// implementation //////////////////////////////////////////////////////////////
+
+namespace test {
+
+    namespace imp {
+
+        template <typename C>
+            inline auto median_(const C& c) -> typename C::value_type {
+                std::sort(std::begin(c), std::end(c));
+                return c.size() % 2 == 0 ? (c[c.size() / 2] + c[c.size() / 2 - 1]) / 2 : c[c.size() / 2];
+            }
+        template <typename C>
+            inline auto median_(C& c) -> typename C::value_type {
+                auto d = c;
+                    std::sort(std::begin(d), std::end(d));
+                return d.size() % 2 == 0 ? (d[d.size() / 2] + d[d.size() / 2 - 1]) / 2 : d[d.size() / 2];
+            }
+        template <typename C>
+            inline auto average_(const C& c) -> typename C::value_type {
+                return std::accumulate(c.begin(), c.end(), typename C::value_type {0}) / c.size();
+            }
+        template <typename C>
+            inline auto min_(const C& c) -> typename C::value_type {
+                return *std::min_element(c.begin(), c.end());
+            }
+        template <typename C>
+            inline auto max_(const C& c) -> typename C::value_type {
+                return *std::max_element(c.begin(), c.end());
+            }
+
+    }
+
+    template <typename B>
         static double measure(B block) {
             using clock = std::chrono::steady_clock;
-            using std::chrono::duration_cast;
-            using std::chrono::nanoseconds;
-            double dur = 0;
-            double t = std::numeric_limits<double>::max();
+            using ns = std::chrono::nanoseconds;
+            std::vector<ns> sl;
+            {   ns  rt = ns {0}, rm = ns {std::chrono::milliseconds {CXON_TIME_RUN_FOR}} ;
                 do {
                     auto const s = clock::now();
-                    block();
-                    auto const n = duration_cast<nanoseconds>(clock::now() - s).count() / 1e6;
-                    dur += n;
-                    t = std::min(t, n);
-                }   while (dur < CXON_TIME_RUN_FOR);
-            return t;
+                        block();
+                    sl.emplace_back(std::chrono::duration_cast<ns>(clock::now() - s));
+                    rt += sl.back();
+                }   while (rt < rm || sl.size() < 17);
+            }
+            auto const md = imp::median_(sl);
+            return std::chrono::duration<double, std::milli>(md).count();
         }
 #   define CXON_MEASURE(...) ::test::measure([&] { __VA_ARGS__ })
 
-    template <typename II>
-        inline auto position(II b, II e)
-            -> typename std::enable_if<!std::is_same<typename std::iterator_traits<II>::iterator_category, std::input_iterator_tag>::value, std::pair<std::size_t, std::size_t>>::type
-        {
-            std::size_t l = 1, c = 1;
-            for ( ; b != e; ++b, ++c)
-                if (*b == '\n')
-                    ++l, c = 0;
-            return std::make_pair(l, c);
-        }
-    template <typename II>
-        inline auto position(II b, II e)
-            -> typename std::enable_if< std::is_same<typename std::iterator_traits<II>::iterator_category, std::input_iterator_tag>::value, std::pair<std::size_t, std::size_t>>::type
-        {
-            return std::make_pair(1, std::distance(b, e));
-        }
+    namespace imp {
 
-    template <typename R, typename I>
-        static std::string format_error(const R& r, I b) {
-            auto const p = position(b, r.end);
+        template <typename II>
+            inline auto position_(II b, II e)
+                -> typename std::enable_if<!std::is_same<typename std::iterator_traits<II>::iterator_category, std::input_iterator_tag>::value, std::pair<std::size_t, std::size_t>>::type
+            {
+                std::size_t l = 1, c = 1;
+                for ( ; b != e; ++b, ++c)
+                    if (*b == '\n')
+                        ++l, c = 0;
+                return std::make_pair(l, c);
+            }
+        template <typename II>
+            inline auto position_(II b, II e)
+                -> typename std::enable_if< std::is_same<typename std::iterator_traits<II>::iterator_category, std::input_iterator_tag>::value, std::pair<std::size_t, std::size_t>>::type
+            {
+                return std::make_pair(1, std::distance(b, e));
+            }
+
+    }
+
+    template <typename R, typename II>
+        static std::string format_error(const R& r, II b) {
+            auto const p = imp::position_(b, r.end);
             return std::string()
                 .append(r.ec.category().name())
                 .append(":(")
