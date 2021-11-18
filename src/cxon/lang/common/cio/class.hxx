607b705f72ef1d70c3ad54ce10c6358588c6ee9e
oknenavin <oknenavin@outlook.com>
2021-11-18 20:53:46 +0200

    - cxon: small optimization of class write helpers (key writing)

--- src/cxon/lang/common/cio/class.hxx
+++ src/cxon/lang/common/cio/class.hxx
@@ -8,7 +8,7 @@
 
 #include "cio.hxx"
 #include "value.hxx"
-#include <cstring> // strcmp
+#include <cstring> // strlen, strncmp
 
 // interface ///////////////////////////////////////////////////////////////////
 
@@ -18,6 +18,7 @@ namespace cxon { namespace cio { namespace cls { // structured types reader/writ
         struct field {
             using type = F;
             char const*const name;
+            std::size_t nlen;
             type const value;
             D dflt;
         };
@@ -51,11 +52,11 @@ namespace cxon { namespace cio { namespace cls {
 
     template <typename S, typename F>
         constexpr auto make_field(const char* name, F f) -> field<F, bool (*)(const S&)> {
-            return { name, f, [](const S&) { return false; } };
+            return { name, std::strlen(name), f, [](const S&) { return false; } };
         }
     template <typename S, typename F, typename D>
         constexpr auto make_field(const char* name, F f, D dflt) -> field<F, D> {
-            return { name, f, dflt };
+            return { name, std::strlen(name), f, dflt };
         }
 
     namespace imp {
@@ -90,7 +91,10 @@ namespace cxon { namespace cio { namespace cls {
             inline auto write_field_(O& o, const S& s, const F& f, Cx& cx)
                 -> enable_if_t<!val::is_sink<typename F::type>::value, bool>
             {
-                return write_key<X>(o, f.name, cx) && write_value<X>(o, field_value_(s, f), cx);
+                return  str::pointer_write<X>(o, f.name, f.nlen, cx) &&
+                            cio::poke<X>(o, X::map::div, cx) &&
+                        write_value<X>(o, field_value_(s, f), cx)
+                ;
             }
         template <typename X, typename O, typename S, typename F, typename Cx>
             constexpr auto write_field_(O&, const S&, const F&, Cx&)
@@ -124,7 +128,7 @@ namespace cxon { namespace cio { namespace cls {
             struct read_ {
                 template <typename S, typename F, typename II, typename Cx>
                     static bool field(S& t, const char* name, const F& fs, int (&st)[L], II& i, II e, Cx& cx) {
-                        return st[N] == 0 && std::strcmp(std::get<N>(fs).name, name) == 0?
+                        return st[N] == 0 && std::strncmp(std::get<N>(fs).name, name, std::get<N>(fs).nlen) == 0 ?
                             (st[N] = 1, read_field<X>(t, std::get<N>(fs), i, e, cx)) :
                             read_<X, N + 1, L>::field(t, name, fs, st, i, e, cx)
                         ;
