17ec3c19c37916c32ce3004bce8680ac8b5aa69e
oknenavin <oknenavin@outlook.com>
2022-02-01 22:42:12 +0200

    - cxon: prepare new release

--- src/cxon/cxon.hxx
+++ src/cxon/cxon.hxx
@@ -7,8 +7,8 @@
 #define CXON_CXON_HXX_
 
 #define CXON_VERSION_MAJOR 0
-#define CXON_VERSION_MINOR 56
-#define CXON_VERSION_PATCH 1
+#define CXON_VERSION_MINOR 57
+#define CXON_VERSION_PATCH 0
 
 #include "utility.hxx"
 
