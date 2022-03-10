24bea6c7c9eba2a800323e782dd2b422795be829
oknenavin <oknenavin@outlook.com>
2022-03-10 23:56:08 +0200

    - cxon: copyright notice update

--- src/cxon/lang/json/lib/boost/variant.hxx
+++ src/cxon/lang/json/lib/boost/variant.hxx
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2021 oknenavin.
+// Copyright (c) 2017-2022 oknenavin.
 // Licensed under the MIT license. See LICENSE file in the library root for full license information.
 //
 // SPDX-License-Identifier: MIT
