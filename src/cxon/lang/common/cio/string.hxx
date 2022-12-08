dee787f2ae80311855a6f23967424ba39a703b07
oknenavin <oknenavin@outlook.com>
2022-12-08 17:36:28 +0200

    - cxon: constraints for string read/write functions - cxon: constraints for cio::write_key string-pointer overload

--- src/cxon/lang/common/cio/string.hxx
+++ src/cxon/lang/common/cio/string.hxx
@@ -17,25 +17,25 @@
 namespace cxon { namespace cio { namespace str { // char arrays: read
 
     template <typename X, typename C, typename II, typename Cx>
-        inline bool string_read(C& c, II& i, II e, Cx& cx);
+        inline auto string_read(C& c, II& i, II e, Cx& cx)      -> enable_if_t<is_char<typename C::value_type>::value, bool>;
 
     template <typename X, typename T, typename II, typename Cx>
-        inline bool array_read(T* f, T* l, II& i, II e, Cx& cx);
+        inline auto array_read(T* f, T* l, II& i, II e, Cx& cx) -> enable_if_t<is_char<T>::value, bool>;
 
     template <typename X, typename T, typename II, typename Cx>
-        inline bool pointer_read(T*& t, II& i, II e, Cx& cx);
+        inline auto pointer_read(T*& t, II& i, II e, Cx& cx)    -> enable_if_t<is_char<T>::value, bool>;
 
 }}}
 
 namespace cxon { namespace cio { namespace str { // char arrays: write
 
     template <typename X, typename O, typename T, typename Cx>
-        inline bool array_write(O& o, const T* t, const T* te, Cx& cx);
+        inline auto array_write(O& o, const T* t, const T* te, Cx& cx)      -> enable_if_t<is_char<T>::value, bool>;
 
     template <typename X, typename O, typename T, typename Cx>
-        inline bool pointer_write(O& o, const T* t, std::size_t s, Cx& cx);
+        inline auto pointer_write(O& o, const T* t, std::size_t s, Cx& cx)  -> enable_if_t<is_char<T>::value, bool>;
     template <typename X, typename O, typename T, typename Cx>
-        inline bool pointer_write(O& o, const T* t, Cx& cx);
+        inline auto pointer_write(O& o, const T* t, Cx& cx)                 -> enable_if_t<is_char<T>::value, bool>;
 
 }}}
 
@@ -160,21 +160,27 @@ namespace cxon { namespace cio { namespace str {
     }
 
     template <typename X, typename C, typename II, typename Cx>
-        inline bool string_read(C& c, II& i, II e, Cx& cx) {
+        inline auto string_read(C& c, II& i, II e, Cx& cx)
+             -> enable_if_t<is_char<typename C::value_type>::value, bool>
+        {
             return  imp::string_read_head_<X>(c, i, e, cx) &&
                     imp::string_read_tail_<X>(c, i, e, cx)
             ;
         }
 
     template <typename X, typename T, typename II, typename Cx>
-        inline bool array_read(T* f, T* l, II& i, II e, Cx& cx) {
+        inline auto array_read(T* f, T* l, II& i, II e, Cx& cx)
+            -> enable_if_t<is_char<T>::value, bool>
+        {
             consume<X>(i, e);
             auto c = cnt::make_range_container(f, l);
             return string_read<X>(c, i, e, cx) && (c.append({}), true);
         }
 
     template <typename X, typename T, typename II, typename Cx>
-        inline bool pointer_read(T*& t, II& i, II e, Cx& cx) {
+        inline auto pointer_read(T*& t, II& i, II e, Cx& cx)
+            -> enable_if_t<is_char<T>::value, bool>
+        {
             consume<X>(i, e);
             if (peek(i, e) == *X::id::nil && consume<X>(X::id::nil, i, e))
                 return true;
@@ -187,21 +193,27 @@ namespace cxon { namespace cio { namespace str {
 namespace cxon { namespace cio { namespace str {
 
     template <typename X, typename O, typename T, typename Cx>
-        inline bool array_write(O& o, const T* f, const T* l, Cx& cx) {
+        inline auto array_write(O& o, const T* f, const T* l, Cx& cx)
+             -> enable_if_t<is_char<T>::value, bool>
+        {
             if (!poke<X>(o, X::string::template del_write<O>, cx)) return false;
                 if (*(l - 1) == T(0)) --l;
             return chr::encode_range<X>(o, f, l, cx) && poke<X>(o, X::string::template del_write<O>, cx);
         }
 
     template <typename X, typename O, typename T, typename Cx>
-        inline bool pointer_write(O& o, const T* t, std::size_t s, Cx& cx) {
+        inline auto pointer_write(O& o, const T* t, std::size_t s, Cx& cx)
+             -> enable_if_t<is_char<T>::value, bool>
+        {
             return  poke<X>(o, X::string::template del_write<O>, cx) &&
                         chr::encode_range<X>(o, t, t + s, cx) &&
                     poke<X>(o, X::string::template del_write<O>, cx)
             ;
         }
     template <typename X, typename O, typename T, typename Cx>
-        inline bool pointer_write(O& o, const T* t, Cx& cx) {
+        inline auto pointer_write(O& o, const T* t, Cx& cx)
+            -> enable_if_t<is_char<T>::value, bool>
+        {
             return t ?
                 pointer_write<X>(o, t, std::char_traits<T>::length(t), cx) :
                 poke<X>(o, X::id::nil, cx)
