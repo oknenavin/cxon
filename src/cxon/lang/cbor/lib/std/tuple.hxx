7f536e3985266fdc87655a20f597a94fd9622779
oknenavin <oknenavin@outlook.com>
2022-12-30 01:36:23 +0200

    - cxon: replace std::tuple_size with sizeof...() where possible

--- src/cxon/lang/cbor/lib/std/tuple.hxx
+++ src/cxon/lang/cbor/lib/std/tuple.hxx
@@ -48,12 +48,12 @@ namespace cxon { namespace cbor { namespace cnt { // container read/write helper
 
     template <typename X, typename II, typename Cx, typename ...T>
         inline bool read_tuple(std::tuple<T...>& t, II& i, II e, Cx& cx) {
-            return imp::tuple_read_<X, std::tuple<T...>, 0, std::tuple_size<std::tuple<T...>>::value>::value(t, i, e, cx);
+            return imp::tuple_read_<X, std::tuple<T...>, 0, sizeof...(T)>::value(t, i, e, cx);
         }
 
     template <typename X, typename O, typename Cx, typename ...T>
         inline bool write_tuple(O& o, const std::tuple<T...>& t, Cx& cx) {
-            return imp::tuple_write_<X, std::tuple<T...>, 0, std::tuple_size<std::tuple<T...>>::value - 1>::value(o, t, cx);
+            return imp::tuple_write_<X, std::tuple<T...>, 0, sizeof...(T) - 1>::value(o, t, cx);
         }
 
 }}}
@@ -75,7 +75,7 @@ namespace cxon {
             template <typename II, typename Cx, typename Y = CBOR<X>>
                 static bool value(std::tuple<T...>& t, II& i, II e, Cx& cx) {
                     return  cbor::tag::read<Y>(i, e, cx) &&
-                            cbor::cnt::read_size_eq<Y>(std::tuple_size<std::tuple<T...>>::value, i, e, cx) &&
+                            cbor::cnt::read_size_eq<Y>(sizeof...(T), i, e, cx) &&
                             cbor::cnt::read_tuple<Y>(t, i, e, cx)
                     ;
                 }
@@ -93,7 +93,7 @@ namespace cxon {
         struct write<CBOR<X>, std::tuple<T...>> {
             template <typename O, typename Cx, typename Y = CBOR<X>>
                 static bool value(O& o, const std::tuple<T...>& t, Cx& cx) {
-                    return  cbor::cnt::write_size<Y>(o, Y::arr, std::tuple_size<std::tuple<T...>>::value, cx) &&
+                    return  cbor::cnt::write_size<Y>(o, Y::arr, sizeof...(T), cx) &&
                             cbor::cnt::write_tuple<Y>(o, t, cx)
                     ;
                 }
