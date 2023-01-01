54c4d155e6b129f8afeeb0d7edae8da12e71a323
oknenavin <oknenavin@outlook.com>
2023-01-01 15:42:33 +0200

    - cxon: update the copyright notice

--- src/cxon/lang/common/cio/key.hxx
+++ src/cxon/lang/common/cio/key.hxx
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2022 oknenavin.
+// Copyright (c) 2017-2023 oknenavin.
 // Licensed under the MIT license. See LICENSE file in the library root for full license information.
 //
 // SPDX-License-Identifier: MIT
