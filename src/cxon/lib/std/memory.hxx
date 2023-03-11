1c03d691e390cd2e6cb8f9b0ba44139bfe6fee41
oknenavin <oknenavin@outlook.com>
2023-03-11 22:17:59 +0200

    - cxon: std::unique_ptr & std::shared_ptr support (arrays not supported)

--- src/cxon/lib/std/memory.hxx
+++ src/cxon/lib/std/memory.hxx
@@ -15,4 +15,45 @@
 #       include "cxon/lang/cbor/lib/std/memory.hxx"
 #   endif
 
+#if defined(CXON_JSON_DEFINED) || defined(CXON_CBOR_DEFINED)
+    namespace cxon {
+
+        template <typename X, typename T, typename D>
+            struct read<X, std::unique_ptr<T[], D>> {
+                template <typename II, typename Cx>
+                    static bool value(std::unique_ptr<T[], D>&, II&, II, Cx&) {
+                        static_assert(unexpected<std::unique_ptr<T[], D>>(), "std::unique_ptr<T[], D> not supported");
+                        return false;
+                    }
+            };
+        template <typename X, typename T, typename D>
+            struct write<X, std::unique_ptr<T[], D>> {
+                template <typename O, typename Cx>
+                    static bool value(O&, const std::unique_ptr<T[], D>&, Cx&) {
+                        static_assert(unexpected<std::unique_ptr<T[], D>>(), "std::unique_ptr<T[], D> not supported");
+                        return false;
+                    }
+            };
+
+        template <typename X, typename T>
+            struct read<X, std::shared_ptr<T[]>> {
+                template <typename II, typename Cx>
+                    static bool value(std::shared_ptr<T[]>&, II&, II, Cx&) {
+                        static_assert(unexpected<std::shared_ptr<T[]>>(), "std::shared_ptr<T[]> not supported");
+                        return false;
+                    }
+            };
+
+        template <typename X, typename T>
+            struct write<X, std::shared_ptr<T[]>> {
+                template <typename O, typename Cx>
+                    static bool value(O&, const std::shared_ptr<T[]>&, Cx&) {
+                        static_assert(unexpected<std::shared_ptr<T[]>>(), "std::shared_ptr<T[]> not supported");
+                        return false;
+                    }
+            };
+
+    }
+#endif
+
 #endif // CXON_LIB_STD_MEMORY_HXX_
