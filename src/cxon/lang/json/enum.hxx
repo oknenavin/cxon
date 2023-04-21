36e0292a88d5289ab283cf62bdae344987519170
oknenavin <oknenavin@outllok.com>
2023-04-21 22:19:21 +0300

    - cxon: fix template parameter shadowing in class/enum implementation macros

--- src/cxon/lang/json/enum.hxx
+++ src/cxon/lang/json/enum.hxx
@@ -13,14 +13,14 @@ namespace cxon { namespace json {
 }}
 
 #define CXON_JSON_ENM_VALUE(T, N, V)   cxon::json::enm::make_value(N, T::V)
-#define CXON_JSON_ENM_VALUE_NAME(N, V) CXON_JSON_ENM_VALUE(T, N, V)
-#define CXON_JSON_ENM_VALUE_ASIS(V)    CXON_JSON_ENM_VALUE(T, #V, V)
+#define CXON_JSON_ENM_VALUE_NAME(N, V) CXON_JSON_ENM_VALUE(CXON_T_, N, V)
+#define CXON_JSON_ENM_VALUE_ASIS(V)    CXON_JSON_ENM_VALUE(CXON_T_, #V, V)
 
 #define CXON_JSON_ENM_READ(Type, ...)\
     namespace cxon {\
         template <typename X, typename II, typename Cx>\
             inline auto read_value(Type& t, II& i, II e, Cx& cx) -> enable_for_t<X, JSON> {\
-                using T = Type;\
+                using CXON_T_ = Type;\
                 static constexpr json::enm::value<Type> v[] = { __VA_ARGS__ };\
                 return json::enm::read_value<X>(t, std::begin(v), std::end(v), i, e, cx);\
             }\
@@ -29,7 +29,7 @@ namespace cxon { namespace json {
     namespace cxon {\
         template <typename X, typename O, typename Cx>\
             inline auto write_value(O& o, const Type& t, Cx& cx) -> enable_for_t<X, JSON> {\
-                using T = Type;\
+                using CXON_T_ = Type;\
                 static constexpr json::enm::value<Type> v[] = { __VA_ARGS__ };\
                 return json::enm::write_value<X>(o, t, std::begin(v), std::end(v), cx);\
             }\
