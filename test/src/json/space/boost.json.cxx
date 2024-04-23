7fbaa1a8c5eedf5d24dec421b68fbef15d228094
oknenavin <oknenavin@outllok.com>
2024-04-24 00:17:38 +0300

    - test: boost::json as a shared library (macos)

--- test/src/json/space/boost.json.cxx
+++ test/src/json/space/boost.json.cxx
@@ -1,7 +1,7 @@
 #include "boost/json.hpp"
 
 int main() {
-    boost::json::error_code ec;
+    boost::system::error_code ec;
     boost::json::value v = boost::json::parse("[42]", ec);
     return !(ec == std::errc{} && v.is_array() && v.as_array().size() == 1 && v.as_array()[0] == 42);
 }
