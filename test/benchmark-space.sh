8d1d1ca1b7ef87d28d3fb99cad8186fcfb37840e
oknenavin <oknenavin@outllok.com>
2024-04-23 22:26:43 +0300

    - test: boost::json as a shared library

--- test/benchmark-space.sh
+++ test/benchmark-space.sh
@@ -2,24 +2,23 @@
 
 printf '# %16s%8s%6s\n' ' ' 'size' 'time'
 
-s10=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s10 src/json/space/cxon.native.c0.cxx 2>&1`
+s10=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/cxon.native.c0.cxx $LDFLAGS -o .out/s10 2>&1`
 printf '%-18s%8s%6s\n' '"cxon (native #0)"' `stat -c%s .out/s10` $s10
 
-s11=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s11 src/json/space/cxon.native.c1.cxx 2>&1`
+s11=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/cxon.native.c1.cxx $LDFLAGS -o .out/s11 2>&1`
 printf '%-18s%8s%6s\n' '"cxon (native #1)"' `stat -c%s .out/s11` $s11
 
-s12=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s12 src/json/space/cxon.native.c2.cxx 2>&1`
+s12=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/cxon.native.c2.cxx $LDFLAGS -o .out/s12 2>&1`
 printf '%-18s%8s%6s\n' '"cxon (native #2)"' `stat -c%s .out/s12` $s12
 
-s13=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s13 src/json/space/cxon.node.cxx 2>&1`
+s13=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/cxon.node.cxx $LDFLAGS -o .out/s13 2>&1`
 printf '%-18s%8s%6s\n' '"cxon (node)"' `stat -c%s .out/s13` $s13
 
-# TODO: BOOST_JSON_STANDALONE deprecated
-/usr/bin/time -f%e -o .out/t20 $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s20  src/json/space/boost.json.cxx 2> /dev/null
-printf '%-18s%8s%6s\n' '"boost/json"' `stat -c%s .out/s20` `cat .out/t20`
+s20=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/boost.json.cxx $LDFLAGS -o .out/s20 2>&1`
+printf '%-18s%8s%6s\n' '"boost/json"' `stat -c%s .out/s20` $s20
 
-s30=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s30  src/json/space/rapidjson.cxx 2>&1`
+s30=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/rapidjson.cxx $LDFLAGS -o .out/s30 2>&1`
 printf '%-18s%8s%6s\n' '"rapidjson"' `stat -c%s .out/s30` $s30
 
-s40=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG $CXXFLAGS -I ../src $CPPFLAGS -o .out/s40  src/json/space/nlohmann.json.cxx 2>&1`
+s40=`/usr/bin/time -f%e $CXX -O2 -s -DNDEBUG -I ../src $CPPFLAGS $CXXFLAGS src/json/space/nlohmann.json.cxx $LDFLAGS -o .out/s40 2>&1`
 printf '%-18s%8s%6s\n' '"nlohmann.json"' `stat -c%s .out/s40` $s40
