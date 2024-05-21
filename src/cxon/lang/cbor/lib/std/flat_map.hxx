095a9172108c27324b39377a872f600d315692a0
oknenavin <oknenavin@outllok.com>
2024-05-21 23:45:41 +0300

    - cxon: c++23: std::flat_map, std::flat_multimap, std::flat_set, std::flat_multiset

---
+++ src/cxon/lang/cbor/lib/std/flat_map.hxx
@@ -0,0 +1,40 @@
+// Copyright (c) 2017-2024 oknenavin.
+// Licensed under the MIT license. See LICENSE file in the library root for full license information.
+//
+// SPDX-License-Identifier: MIT
+
+#ifndef CXON_CBOR_LIB_STD_FLAT_MAP_HXX_
+#define CXON_CBOR_LIB_STD_FLAT_MAP_HXX_
+
+#include "cxon/lang/cbor/common/container.hxx"
+#include "imp/map.hxx"
+
+namespace cxon { namespace cnt {
+
+    template <typename X, typename K, typename V, typename ...R>
+        struct element_reader<CBOR<X>, std::flat_map<K, V, R...>>        : cbor::imp::map_element_reader_<CBOR<X>, std::flat_map<K, V, R...>> {};
+    template <typename X, typename K, typename V, typename ...R>
+        struct element_writer<CBOR<X>, std::flat_map<K, V, R...>>        : cbor::imp::map_element_writer_<CBOR<X>, std::flat_map<K, V, R...>> {};
+
+    template <typename X, typename K, typename V, typename ...R>
+        struct element_reader<CBOR<X>, std::flat_multimap<K, V, R...>>   : cbor::imp::map_element_reader_<CBOR<X>, std::flat_multimap<K, V, R...>> {};
+    template <typename X, typename K, typename V, typename ...R>
+        struct element_writer<CBOR<X>, std::flat_multimap<K, V, R...>>   : cbor::imp::map_element_writer_<CBOR<X>, std::flat_multimap<K, V, R...>> {};
+
+}}
+
+namespace cxon {
+
+    template <typename X, typename K, typename V, typename ...R>
+        struct read<CBOR<X>, std::flat_map<K, V, R...>>                  : cbor::imp::map_reader_<CBOR<X>, std::flat_map<K, V, R...>> {};
+    template <typename X, typename K, typename V, typename ...R>
+        struct write<CBOR<X>, std::flat_map<K, V, R...>>                 : cbor::imp::map_writer_<CBOR<X>, std::flat_map<K, V, R...>> {};
+
+    template <typename X, typename K, typename V, typename ...R>
+        struct read<CBOR<X>, std::flat_multimap<K, V, R...>>             : cbor::imp::map_reader_<CBOR<X>, std::flat_multimap<K, V, R...>> {};
+    template <typename X, typename K, typename V, typename ...R>
+        struct write<CBOR<X>, std::flat_multimap<K, V, R...>>            : cbor::imp::map_writer_<CBOR<X>, std::flat_multimap<K, V, R...>> {};
+
+}
+
+#endif // CXON_CBOR_LIB_STD_FLAT_MAP_HXX_
