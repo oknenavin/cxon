a0d38e3d6cb083ea7ab1575532a0348cb4a2230b
oknenavin <oknenavin@outllok.com>
2024-06-14 22:37:54 +0300

    - test: fast fail benchmarks

--- test/src/json/json.node-time.native.cxx
+++ test/src/json/json.node-time.native.cxx
@@ -27,6 +27,11 @@ namespace test { namespace kind {
                 t.size = json.size() - 1;
             // read
                 std::vector<T> vo;
+                {   // check
+                    vo.emplace_back();
+                    auto const r = cxon::from_bytes<TIME>(vo.back(), json);
+                        if (!r) return t.error = format_error(r, json.begin()), void();
+                }
                 t.time.read.push_back(CXON_MEASURE(
                     vo.emplace_back();
                     auto const r = cxon::from_bytes<TIME>(vo.back(), json);
@@ -35,6 +40,11 @@ namespace test { namespace kind {
             // write
                 auto o = std::move(vo.back()); vo.clear();
                 std::vector<std::string> vs;
+                {   // check
+                    vs.emplace_back();
+                    auto r = cxon::to_bytes<TIME>(vs.back(), o);
+                        if (!r) return t.error = format_error(r, vs.back().begin()), void();
+                }
                 std::array<char, 4 * 1024> bf;
                 t.time.write.push_back(CXON_MEASURE(
                     vs.emplace_back();
