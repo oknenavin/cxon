0c00f74d414e4e530e2f9feb9c430e24613319d6
oknenavin <oknenavin@outllok.com>
2024-07-06 21:38:36 +0300

    - test: add json native diff tests

--- test/src/cbor/cbor.node-main.cxx
+++ test/src/cbor/cbor.node-main.cxx
@@ -253,8 +253,8 @@ namespace test { namespace kind { // round-trip
                     ++err, std::fprintf(stderr, "error: cannot be opened: '%s'\n", file.c_str());
                     continue;
                 }
-            std::string const f0 = name(file) + ".ct(0).json";
-            std::string const f1 = name(file) + ".ct(1).json";
+            std::string const f0 = "cbor.round-trip." + name(file) + ".(0).json";
+            std::string const f1 = "cbor.round-trip." + name(file) + ".(1).json";
             {   // 0
                 std::ofstream os(f0, std::ofstream::binary);
                     if (!os) {
--- test/src/json/json.node-time.native.cxx
+++ test/src/json/json.node-time.native.cxx
@@ -3,18 +3,13 @@
 //
 // SPDX-License-Identifier: MIT
 
-
 #include "json.node-time.hxx"
 #include "json.node-time.native.hxx"
 
-#include "cxon/lib/node.ordered.hxx"
-
-#include <array>
 #include <fstream>
 
-////////////////////////////////////////////////////////////////////////////////
 
-using node = cxon::json::ordered_node;
+////////////////////////////////////////////////////////////////////////////////
 
 namespace test { namespace kind {
 
@@ -52,55 +47,57 @@ namespace test { namespace kind {
                 ));
         }
 
-    using executor = void (*)(test& t);
+    namespace native {
 
-    static std::map<std::string, executor> executors_ = {
-        // set 1 (time-s1.in)
-        { "blns.json",              &time_run<blns::object> },
-        { "emoji.json",             &time_run<emoji::object> },
-        { "apache_builds.json",     &time_run<apache_builds::object> },
-        { "canada.json",            &time_run<canada::object> },
-#       ifdef CXON_HAS_LIB_STD_OPTIONAL
-        { "citm_catalog.json",      &time_run<citm_catalog::object> },
-#       endif
-#       ifdef CXON_HAS_LIB_STD_OPTIONAL
-        { "github_events.json",     &time_run<github_events::object> },
-#       endif
-        { "gsoc-2018.json",         &time_run<gsoc_2018::object> },
-#       ifdef CXON_HAS_LIB_STD_OPTIONAL
-        { "instruments.json",       &time_run<instruments::object> },
-#       endif
-        { "marine_ik.json",         &time_run<marine_ik::object> },
-        { "mesh.json",              &time_run<mesh::object> },
-        { "mesh.pretty.json",       &time_run<mesh::object> },
-        { "numbers.json",           &time_run<numbers::object> },
-        { "random.json",            &time_run<random::object> },
-#       ifdef CXON_HAS_LIB_STD_OPTIONAL
-        { "twitter.json",           &time_run<twitter::object> },
-        { "twitterescaped.json",    &time_run<twitter::object> },
-#       endif
-        { "update-center.json",     &time_run<update_center::object> },
-        // set 2 (time-s2.in)
-        { "random-u8.json",         &time_run<synth::object_u8> },
-        { "random-s8.json",         &time_run<synth::object_s8> },
-        { "random-u16.json",        &time_run<synth::object_u16> },
-        { "random-s16.json",        &time_run<synth::object_s16> },
-        { "random-u32.json",        &time_run<synth::object_u32> },
-        { "random-s32.json",        &time_run<synth::object_s32> },
-        { "random-u64.json",        &time_run<synth::object_u64> },
-        { "random-s64.json",        &time_run<synth::object_s64> },
-        { "random-fp32.json",       &time_run<synth::object_fp32> },
-        { "random-fp64.json",       &time_run<synth::object_fp64> },
-        { "random-null.json",       &time_run<synth::object_null> },
-        { "random-bool.json",       &time_run<synth::object_bool> },
-        { "random-string.json",     &time_run<synth::object_string> },
-        { "random-array.json",      &time_run<synth::object_array> },
-        { "random-object.json",     &time_run<synth::object_object> }
-    };
+        static std::map<std::string, void (*)(test&)> executors_ = {
+            // set 1 (time-s1.in)
+            { "blns.json",              &time_run<blns::object> },
+            { "emoji.json",             &time_run<emoji::object> },
+            { "apache_builds.json",     &time_run<apache_builds::object> },
+            { "canada.json",            &time_run<canada::object> },
+#           ifdef CXON_HAS_LIB_STD_OPTIONAL
+            { "citm_catalog.json",      &time_run<citm_catalog::object> },
+#           endif
+#           ifdef CXON_HAS_LIB_STD_OPTIONAL
+            { "github_events.json",     &time_run<github_events::object> },
+#           endif
+            { "gsoc-2018.json",         &time_run<gsoc_2018::object> },
+#           ifdef CXON_HAS_LIB_STD_OPTIONAL
+            { "instruments.json",       &time_run<instruments::object> },
+#           endif
+            { "marine_ik.json",         &time_run<marine_ik::object> },
+            { "mesh.json",              &time_run<mesh::object> },
+            { "mesh.pretty.json",       &time_run<mesh::object> },
+            { "numbers.json",           &time_run<numbers::object> },
+            { "random.json",            &time_run<random::object> },
+#           ifdef CXON_HAS_LIB_STD_OPTIONAL
+            { "twitter.json",           &time_run<twitter::object> },
+            { "twitterescaped.json",    &time_run<twitter::object> },
+#           endif
+            { "update-center.json",     &time_run<update_center::object> },
+            // set 2 (time-s2.in)
+            { "random-u8.json",         &time_run<synth::object_u8> },
+            { "random-s8.json",         &time_run<synth::object_s8> },
+            { "random-u16.json",        &time_run<synth::object_u16> },
+            { "random-s16.json",        &time_run<synth::object_s16> },
+            { "random-u32.json",        &time_run<synth::object_u32> },
+            { "random-s32.json",        &time_run<synth::object_s32> },
+            { "random-u64.json",        &time_run<synth::object_u64> },
+            { "random-s64.json",        &time_run<synth::object_s64> },
+            { "random-fp32.json",       &time_run<synth::object_fp32> },
+            { "random-fp64.json",       &time_run<synth::object_fp64> },
+            { "random-null.json",       &time_run<synth::object_null> },
+            { "random-bool.json",       &time_run<synth::object_bool> },
+            { "random-string.json",     &time_run<synth::object_string> },
+            { "random-array.json",      &time_run<synth::object_array> },
+            { "random-object.json",     &time_run<synth::object_object> }
+        };
+
+    }
 
     void time_cxon_native(test& t) {
-        auto i = executors_.find(t.source.substr(t.source.rfind('/') + 1));
-        i != executors_.end() ?
+        auto i = native::executors_.find(t.source.substr(t.source.rfind('/') + 1));
+        i != native::executors_.end() ?
             i->second(t) :
             (time_cxon_node(t), t.flag = true, void())
         ;
