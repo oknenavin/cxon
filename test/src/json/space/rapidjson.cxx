5926b94d97d9830949a7235f30c8b05b00008689
oknenavin <oknenavin@outllok.com>
2024-07-22 22:39:24 +0300

    - json: documentation update

--- test/src/json/space/rapidjson.cxx
+++ test/src/json/space/rapidjson.cxx
@@ -2,7 +2,7 @@
 
 int main() {
     rapidjson::Document d;
-    auto constexpr opt = rapidjson::kParseValidateEncodingFlag|rapidjson::kParseFullPrecisionFlag;
+        auto constexpr opt = rapidjson::kParseValidateEncodingFlag|rapidjson::kParseFullPrecisionFlag;
     rapidjson::ParseResult r = d.Parse<opt>("[42]");
     return !(r && d.IsArray() && d.GetArray().Size() == 1 && d.GetArray()[0] == 42);
 }
