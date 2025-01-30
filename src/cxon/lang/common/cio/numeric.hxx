2d4386b3ed63412d758a25c53681ee7e487938e6
oknenavin <oknenavin@outlook.com>
2025-01-30 21:17:57 +0200

    - cxon: update the copyright notice

--- src/cxon/lang/common/cio/numeric.hxx
+++ src/cxon/lang/common/cio/numeric.hxx
@@ -1,4 +1,4 @@
-// Copyright (c) 2017-2024 oknenavin.
+// Copyright (c) 2017-2025 oknenavin.
 // Licensed under the MIT license. See LICENSE file in the library root for full license information.
 //
 // SPDX-License-Identifier: MIT
